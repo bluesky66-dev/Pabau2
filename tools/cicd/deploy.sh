c3b4c0f05fedd9f1c46c18f727f84f30a9188c57
James Colton <james.c@pabau.com>
2020-12-08 11:34:43 +0000

    deploy script

--- tools/cicd/deploy.sh
+++ tools/cicd/deploy.sh
@@ -6,6 +6,15 @@ echo "About to deploy!"
 echo "Hostname: $HOSTNAME"
 echo "Hostname: $(hostname)"
 
+mkdir -p /cdata/monorepo
 
+if [ ! -d "/cdata/monorepo/.git" ]; then
+  git clone git@bitbucket.org:pabau/monorepo .
+fi
+
+git pull --ff-only
+docker build -t pabau-app-frontend crm/web/
+
+~/rancher-cli/bin/rancher ps
 
 echo "EOF"
