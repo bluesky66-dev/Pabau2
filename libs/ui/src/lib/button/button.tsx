e9973eea7eb5feea1b7eb1a425e1b6f4e202e676
James Colton <james.c@pabau.com>
2020-12-14 17:22:27 +0000

    better husky with nx + minor storybook update

--- libs/ui/src/lib/button/button.tsx
+++ libs/ui/src/lib/button/button.tsx
@@ -10,7 +10,7 @@ export interface ButtonProps {
 export function Button2({ disabled, children, onClick }: ButtonProps) {
   return (
     <AntButton disabled={disabled} onClick={() => onClick?.()}>
-      {children}swuellertd
+      {children}swuellertds
     </AntButton>
   )
 }
