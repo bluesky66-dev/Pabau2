98ffb94aa1987385d29c1da52e66f093921526e3
James Colton <james.c@pabau.com>
2020-12-15 23:58:00 +0000

    git

--- tools/cicd/storybook.deploy.sh
+++ tools/cicd/storybook.deploy.sh
@@ -25,6 +25,8 @@ git pull --ff-only
 
 echo "About to docker build..."
 docker build -t pabau-ui-storybook -f tools/cicd/storybook.Dockerfile .
+docker tag pabau/pabau-ui-storybook:latest 10.42.184.17:5000/pabau/pabau-ui-storybook:latest
+docker push 10.42.184.17:5000/pabau/pabau-ui-storybook
 
 echo "About to deploy..."
 docker run --rm -e "RANCHER_ACCESS_KEY=${RANCHER_ACCESS_KEY}" -e "RANCHER_SECRET_KEY=${RANCHER_SECRET_KEY}" -e "RANCHER_URL=${RANCHER_URL}" cdrx/rancher-gitlab-deploy upgrade --environment Default --stack global-ops --create --service pabau-ui-storybook --finish-upgrade --rollback-on-error --start-before-stopping --wait-for-upgrade-to-finish --sidekicks
