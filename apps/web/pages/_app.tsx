7254738d796ce51c999a44c6b3b559e8e82aaaf6
James Colton <james.c@pabau.com>
2020-12-16 00:34:23 +0000

    apollo graphql

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,7 +1,131 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import './styles.less'
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from '@apollo/client'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { OperationDefinitionNode } from 'graphql'
+
+const cache = new InMemoryCache()
+const GRAPHQL_ENDPOINT = 'wss://api.new.pabau.com/v1/graphql'
+
+// const request = async (operation) => {
+//   operation.setContext({
+//     http: {
+//       includeExtensions: true,
+//       includeQuery: false,
+//     },
+//     headers,
+//   })
+// }
+//
+// const requestLink = new ApolloLink(
+//   (operation, forward) =>
+//     new Observable((observer) => {
+//       let handle
+//       Promise.resolve(operation)
+//         .then((oper) => request(oper))
+//         .then(() => {
+//           handle = forward(operation).subscribe({
+//             next: observer.next.bind(observer),
+//             error: observer.error.bind(observer),
+//             complete: observer.complete.bind(observer),
+//           })
+//         })
+//         .catch(observer.error.bind(observer))
+//
+//       return () => {
+//         if (handle) handle.unsubscribe()
+//       }
+//     })
+// )
+
+// const httpLink = new BatchHttpLink({
+//   uri: GRAPHQL_ENDPOINT,
+// })
+
+const httpLink = new HttpLink({
+  uri: 'https://api.new.pabau.com/v1/graphql',
+})
+
+const wsLink = process.browser
+  ? new WebSocketLink({
+      uri: GRAPHQL_ENDPOINT,
+      options: {
+        reconnect: true,
+        connectionParams: {
+          authToken: 'user.authToken goes here',
+        },
+      },
+    })
+  : null
+
+// Let Apollo figure out if the request is over ws or http
+const terminatingLink = wsLink
+  ? split(
+      ({ query }) => {
+        const { kind, operation } = getMainDefinition(query) as OperationDefinitionNode
+        return kind === 'OperationDefinition' && operation === 'subscription' && process.browser
+      },
+      wsLink,
+      httpLink
+    )
+  : httpLink
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    // onError(({ graphQLErrors, networkError }) => {
+    //   if (graphQLErrors) {
+    //     console.error({ graphQLErrors })
+    //   }
+    //   if (networkError) {
+    //     console.error({ networkError })
+    //   }
+    // }),
+    terminatingLink,
+
+    // link,
+
+    // withClientState({
+    //   defaults: {
+    //     isConnected: true,
+    //   },
+    //   resolvers: {
+    //     Mutation: {
+    //       updateNetworkStatus: (_, { isConnected }, { cache }) => {
+    //         cache.writeData({ data: { isConnected } })
+    //         return null
+    //       },
+    //     },
+    //   },
+    //   cache,
+    // }),
+
+    // Push the links into the Apollo client
+    // createPersistedQueryLink({ generateHash: ({}) => documentId }).concat(
+    //   // New config
+    //   terminatingLink
+    //   // Old config
+    //   // new BatchHttpLink({
+    //   //   uri: GRAPHQL_ENDPOINT,
+    //   //   credentials: 'include'
+    //   // })
+    // ),
+  ]),
+  cache,
+})
 
 export default function CustomApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
--- apps/web/pages/marketing/sources/index.tsx
+++ apps/web/pages/marketing/sources/index.tsx
@@ -2,37 +2,39 @@ import React, { FC } from 'react'
 import './styles.less'
 import { Table } from 'antd'
 import { Layout } from '@pabau/ui'
-import { useRouter } from 'next/router'
+import { gql, useSubscription } from '@apollo/client'
+
+const MARKETING_SOURCE_LIST = gql`
+  subscription MarketingSourceList {
+    marketing_source {
+      id
+      name
+    }
+  }
+`
 
 export const Index: FC = () => {
-  const router = useRouter()
-  useEffect(() => {
-    window.document.onkeypress = (e) => {
-      console.log(e)
-      if (e.code === 'KeyN') {
-        setTimeout(() => router.push('sources/new'), 1)
-      }
-    }
-  })
+  const { loading, error, data } = useSubscription(MARKETING_SOURCE_LIST)
+
+  //if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
   return (
     <Layout pageTitle="Marketing Sources" newButtonText="New Source" onNewClicked="new">
       <Table
+        loading={loading}
         style={{ height: '100vh' }}
         sticky={true}
+        pagination={data?.marketing_source?.length > 10 ? {} : false}
         columns={[
           {
             title: 'name',
             dataIndex: 'name',
           },
         ]}
-        dataSource={[
-          {
-            name: 'William',
-          },
-        ]}
+        dataSource={data?.marketing_source.map((e) => ({ key: e.id, ...e }))}
       />
     </Layout>
   )
 }
-
 export default Index
