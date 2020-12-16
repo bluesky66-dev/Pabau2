593547413f5aa0a940d77461280ac1dd1bf30e9f
James Colton <james.c@pabau.com>
2020-12-16 00:13:34 +0000

    git deploy

--- tools/cicd/storybook.deploy.sh
+++ tools/cicd/storybook.deploy.sh
@@ -15,7 +15,7 @@ pwd && ls -al
 
 echo "About to checkout..."
 if [ ! -d "/cdata/monorepo/.git" ]; then
-  echo "Checking out..."deploy.sh
+  echo "Checking out..."
   git clone git@bitbucket.org:pabau/monorepo .
 fi
 
@@ -25,10 +25,29 @@ git pull --ff-only
 
 echo "About to docker build..."
 docker build -t pabau/pabau-ui-storybook:latest -f tools/cicd/storybook.Dockerfile .
+
+echo "About to docker tag..."
 docker tag pabau/pabau-ui-storybook:latest 10.42.184.17:5000/pabau/pabau-ui-storybook:latest
+
+echo "About to docker push..."
 docker push 10.42.184.17:5000/pabau/pabau-ui-storybook
 
 echo "About to deploy..."
-docker run --rm -e "RANCHER_ACCESS_KEY=${RANCHER_ACCESS_KEY}" -e "RANCHER_SECRET_KEY=${RANCHER_SECRET_KEY}" -e "RANCHER_URL=${RANCHER_URL}" cdrx/rancher-gitlab-deploy upgrade --environment Default --stack global-ops --create --service pabau-ui-storybook --finish-upgrade --rollback-on-error --start-before-stopping --wait-for-upgrade-to-finish --sidekicks
+docker run --rm \
+  -e "RANCHER_ACCESS_KEY=${RANCHER_ACCESS_KEY}" \
+  -e "RANCHER_SECRET_KEY=${RANCHER_SECRET_KEY}" \
+  -e "RANCHER_URL=${RANCHER_URL}" \
+  cdrx/rancher-gitlab-deploy \
+    upgrade \
+      --environment Default \
+      --stack global-ops \
+      --create \
+      --service pabau-ui-storybook \
+      --new-image "10.42.184.17:5000/pabau/pabau-ui-storybook:latest" \
+      --finish-upgrade \
+      --rollback-on-error \
+      --start-before-stopping \
+      --wait-for-upgrade-to-finish \
+      --sidekicks
 
 echo "EOF"
