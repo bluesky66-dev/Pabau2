055c925d0dee3b712c9f22cffe7830824cb2faeb
James Colton <james.c@pabau.com>
2020-12-16 00:20:32 +0000

    git deploy

--- tools/cicd/web.deploy.sh
+++ tools/cicd/web.deploy.sh
@@ -24,9 +24,30 @@ git pull --ff-only
 
 
 echo "About to docker build..."
-docker build -t pabau-app-frontend -f tools/cicd/web.Dockerfile .
+docker build -t pabau/newpabau-web:latest -f tools/cicd/web.Dockerfile .
+
+echo "About to docker tag..."
+docker tag pabau/newpabau-web:latest 10.42.184.17:5000/pabau/newpabau-web:latest
+
+echo "About to docker push..."
+docker push 10.42.184.17:5000/pabau/newpabau-web
 
 echo "About to deploy..."
-docker run --rm -e "RANCHER_ACCESS_KEY=${RANCHER_ACCESS_KEY}" -e "RANCHER_SECRET_KEY=${RANCHER_SECRET_KEY}" -e "RANCHER_URL=${RANCHER_URL}" cdrx/rancher-gitlab-deploy upgrade --environment Default --stack global-ops --create --service newpabau-web --finish-upgrade --rollback-on-error --start-before-stopping --wait-for-upgrade-to-finish --sidekicks
+docker run --rm \
+  -e "RANCHER_ACCESS_KEY=${RANCHER_ACCESS_KEY}" \
+  -e "RANCHER_SECRET_KEY=${RANCHER_SECRET_KEY}" \
+  -e "RANCHER_URL=${RANCHER_URL}" \
+  cdrx/rancher-gitlab-deploy \
+    upgrade \
+      --environment Default \
+      --stack global-ops \
+      --create \
+      --service newpabau-web \
+      --new-image "10.42.184.17:5000/pabau/newpabau-web:latest" \
+      --finish-upgrade \
+      --rollback-on-error \
+      --start-before-stopping \
+      --wait-for-upgrade-to-finish \
+      --sidekicks
 
 echo "EOF"
