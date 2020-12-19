79d0b1ec1f7efe1e1dd0cd03cb8c297883bcada3
James Colton <james.c@pabau.com>
2020-12-19 14:38:07 +0000

    backend: add logging

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { yup } from '@pabau/yup'
+import logger from '../lib/logger'
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    logger("test!");
     return { message: 'Welcome to backend! This should say yup --> ' + yup() }
   }
+
+
 }
