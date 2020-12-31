84e52d3f9a6ab2ffd05f362c80703fab9d549401
Dipak Savaliya <dipak241@gmail.com>
2020-12-31 17:55:32 +0530

    Heder search popover story

--- libs/ui/src/lib/header/Header.stories.tsx
+++ libs/ui/src/lib/header/Header.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 // import { Header2 } from './Header'
 import { PabauHeader } from './PabauHeader'
 import { Logo } from './logo/Logo'
+import { Search } from './search/search'
 
 export default {
   component: PabauHeader,
@@ -14,4 +15,8 @@ export const PabauHeaderStory = () => {
 }
 export const PabauLogo = () => {
   return <Logo />
+}
+
+export const PabauSearch = () => {
+  return <Search />
 }
