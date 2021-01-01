70512c3e50f6dd6cdccf0862879c3c9d75ef87cf
Dipak Savaliya <dipak241@gmail.com>
2021-01-01 15:43:28 +0530

    Avatar dropdown design fixes

--- libs/ui/src/lib/dropdown/dropdown.tsx
+++ libs/ui/src/lib/dropdown/dropdown.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Menu, Dropdown as AntDropDown, Button } from 'antd'
+import React, { useState } from 'react'
+import { Menu, Button, Popover } from 'antd'
 import './dropdown.less'
-import Logo from '../pabaucliniclogo/Logo'
 import LaunchSVG from './LaunchSVG'
 import {
+  RightOutlined,
   UserOutlined,
   NotificationOutlined,
   QuestionCircleOutlined,
@@ -15,12 +15,11 @@ import {
   ExclamationOutlined,
   PlaySquareOutlined,
 } from '@ant-design/icons'
+import { ReactComponent as PABAULOGO } from './company/pabaulogo.svg'
 import { ReactComponent as JASVG } from './company/JA.svg'
 import { ReactComponent as UPSVG } from './UP.svg'
 import { ReactComponent as TaskSVG } from './Vector.svg'
 import { languageMenu } from './language/lang-logos/index'
-
-const { SubMenu } = Menu
 
 export interface DropDownInterface {
   loading?: boolean
@@ -29,24 +28,79 @@ export interface DropDownInterface {
   handleSubmit?: () => void
 }
 
-const menu = (
-  <Menu className="avatar-menu">
-    <SubMenu
-      key="clinicMenu"
-      icon={<Logo />}
-      title="Pabau Clinic Software"
-      className="pabau-clinic-header"
-      popupClassName="sub-clinic-menu"
-    >
-      <Menu.Item className="sub-dropdown-menu">
-        <div className="sub-dropdown-header">
+export function Dropdown(): JSX.Element {
+  const [activeMenu, setActiveMenu] = useState('Menu')
+
+  const menu = (
+    <Menu className="avatar-menu">
+      <Menu.Item className="dropdown-menu" onClick={() => onClickAvatarMenu('ClinicMenu')}>
+        <div className="dropdown-header">
+          <PABAULOGO />
+          <span className="header-text">Pabau Clinic Software</span>
+        </div>
+        <RightOutlined className="dropdown-icon" />
+      </Menu.Item>
+      <Menu.Item className="userinfo">
+        <div className="user-name">William Branham</div>
+        <div className="user-balance">
+          <p>Balance</p>
+          <span>9445,00</span>
+        </div>
+      </Menu.Item>
+      <Menu.Item className="dropdown-menu">
+        <div className="dropdown-header">
+          <UserOutlined style={{ color: '#9292A3' }} />
+          <span className="header-text">Account Settings</span>
+        </div>
+        <LaunchSVG />
+      </Menu.Item>
+      <Menu.Item className="dropdown-menu" style={{ borderBottom: '1px solid #F1F1F1' }}>
+        <div className="dropdown-header">
+          <TaskSVG />
+          <span className="header-text task-text">Tasks</span>
+        </div>
+      </Menu.Item>
+      <Menu.Item className="dropdown-menu" onClick={() => onClickAvatarMenu('FeedbackMenu')}>
+        <div className="dropdown-header">
+          <NotificationOutlined className="dropdown-icon" />
+          <span className="header-text">Give feedback</span>
+        </div>
+        <RightOutlined className="dropdown-icon" />
+      </Menu.Item>
+      <Menu.Item className="dropdown-menu" onClick={() => onClickAvatarMenu('HelpMenu')}>
+        <div className="dropdown-header">
+          <QuestionCircleOutlined className="dropdown-icon" />
+          <span className="header-text">Help & Support</span>
+        </div>
+        <RightOutlined className="dropdown-icon" />
+      </Menu.Item>
+      <Menu.Item className="dropdown-menu" onClick={() => onClickAvatarMenu('LangMenu')}>
+        <div className="dropdown-header">
+          <GlobalOutlined className="dropdown-icon" />
+          <span className="header-text">English</span>
+        </div>
+        <RightOutlined className="dropdown-icon" />
+      </Menu.Item>
+      <Menu.Item className="dropdown-menu">
+        <div className="dropdown-header">
+          <ExportOutlined className="dropdown-icon" />
+          <span className="header-text">Log out</span>
+        </div>
+      </Menu.Item>
+    </Menu>
+  )
+
+  const ClinicSubMenu = (
+    <Menu className="avatar-sub-menu">
+      <Menu.Item className="sub-dropdown-list" onClick={() => onClickAvatarMenu('Menu')}>
+        <div className="sub-dropdown-list-header">
           <LeftOutlined className="sub-logo" />
           <span className="sub-header-text">Select company</span>
         </div>
       </Menu.Item>
       <Menu.Item className="sub-dropdown-list">
         <div className="sub-dropdown-list-header">
-          <Logo />
+          <PABAULOGO />
           <span className="sub-dropdown-list-header-text text-nowrap active-menu">
             Pabau Clinic Software
           </span>
@@ -69,96 +123,67 @@ const menu = (
           </span>
         </div>
       </Menu.Item>
-    </SubMenu>
-    <Menu.Item className="userinfo">
-      <div className="user-name">William Branham</div>
-      <div className="user-balance">
-        <p>Balance</p>
-        <span>9445,00</span>
-      </div>
-    </Menu.Item>
-    <Menu.Item className="dropdown-menu">
-      <div className="dropdown-header">
-        <UserOutlined className="logo" />
-        <span className="header-text">Account Settings</span>
-      </div>
-      <LaunchSVG />
-    </Menu.Item>
-    <Menu.Item className="dropdown-menu" style={{ borderBottom: '1px solid #F1F1F1' }}>
-      <div className="dropdown-header">
-        <TaskSVG />
-        <span className="header-text">Tasks</span>
-      </div>
-    </Menu.Item>
-    <SubMenu
-      key="feedbackMenu"
-      icon={<NotificationOutlined className="submenu-header-icon" />}
-      title="Give feedback"
-      className="submenu-header"
-      popupClassName="feedback-menu"
-    >
-      <Menu.Item className="">
-        <div className="popup-heading-style">
-          <LeftOutlined className="" />
-          <span className="">Give us feedback</span>
+    </Menu>
+  )
+
+  const FeedbackMenu = (
+    <Menu className="avatar-help-menu">
+      <Menu.Item className="avatar-help-sub-list" onClick={() => setActiveMenu('Menu')}>
+        <div className="feedback-align-content">
+          <LeftOutlined className="sub-logo" />
+          <p className="sub-header-text">Give us feedback</p>
         </div>
       </Menu.Item>
-      <Menu.Item className="">
-        <div className="feedback-text-style">
+      <Menu.Item className="avatar-help-sub-list">
+        <div className="feedback-align-content">
           <InfoCircleOutlined className="" />
           <span className="">Help us improve Pabau</span>
         </div>
       </Menu.Item>
-      <Menu.Item className="">
-        <div className="feedback-text-style">
+      <Menu.Item className="avatar-help-sub-list">
+        <div className="feedback-align-content">
           <ExclamationOutlined className="" />
           <span className="">Something went wrong</span>
         </div>
       </Menu.Item>
-    </SubMenu>
-    <SubMenu
-      key="helpMenu"
-      icon={<QuestionCircleOutlined className="submenu-header-icon" />}
-      title="Help & Support"
-      className="submenu-header"
-      popupClassName="help-menu"
-    >
-      <Menu.Item className="">
-        <div className="popup-heading-style">
-          <LeftOutlined className="" />
-          <span className="">Help & Support </span>
+    </Menu>
+  )
+
+  const HelpMenu = (
+    <Menu className="avatar-help-menu">
+      <Menu.Item className="avatar-help-sub-list" onClick={() => onClickAvatarMenu('Menu')}>
+        <div className="feedback-align-content">
+          <LeftOutlined className="sub-logo" />
+          <p className="sub-header-text">Help & Support </p>
         </div>
       </Menu.Item>
-      <Menu.Item className="">
-        <div className="feedback-text-style">
+      <Menu.Item className="avatar-help-sub-list">
+        <div className="feedback-align-content">
           <QuestionCircleOutlined className="" />
           <span className="">Help Centre</span>
         </div>
       </Menu.Item>
-      <Menu.Item className="">
-        <div className="feedback-text-style">
+      <Menu.Item className="avatar-help-sub-list">
+        <div className="feedback-align-content">
           <PlaySquareOutlined className="" />
           <span className="">Video Guides</span>
         </div>
       </Menu.Item>
-      <Menu.Item className="">
-        <div className="feedback-text-style">
+      <Menu.Item className="avatar-help-sub-list">
+        <div className="feedback-align-content">
           <ExclamationOutlined className="" />
           <span className="">Contact Support</span>
         </div>
       </Menu.Item>
-    </SubMenu>
-    <SubMenu
-      key="langMenu"
-      icon={<GlobalOutlined className="submenu-header-icon" />}
-      title="English"
-      className="submenu-header"
-      popupClassName="lang-menu"
-    >
-      <Menu.Item className="">
-        <div className="popup-heading-style">
+    </Menu>
+  )
+
+  const LangMenu = (
+    <Menu className="avatar-help-menu">
+      <Menu.Item className="lang-sub-dropdown-menu" onClick={() => onClickAvatarMenu('Menu')}>
+        <div className="lang-align-content">
           <LeftOutlined className="" />
-          <span className="">Select language </span>
+          <p className="">Select language </p>
         </div>
       </Menu.Item>
       {languageMenu.map((lang, index) => {
@@ -166,27 +191,44 @@ const menu = (
           <Menu.Item key={index} className="language-text-align">
             <div className="language-flag-center">
               {lang.logo}
-              <span className={`${lang.selected && 'active-menu'}`}>{lang.label}</span>
+              <span className={`${lang.selected ? 'active-menu' : ''}`}>{lang.label}</span>
             </div>
             {lang.selected && <CheckCircleFilled className="check-icon active-menu" />}
           </Menu.Item>
         )
       })}
-    </SubMenu>
-    <Menu.Item className="dropdown-menu">
-      <div className="dropdown-header">
-        <ExportOutlined className="logo" />
-        <span className="header-text">Log out</span>
-      </div>
-    </Menu.Item>
-  </Menu>
-)
+    </Menu>
+  )
 
-export function Dropdown(): JSX.Element {
+  const onClickAvatarMenu = (menuName: string) => {
+    setActiveMenu(menuName)
+  }
+
+  const avatarMenu = () => {
+    if (activeMenu === 'Menu') {
+      return menu
+    } else if (activeMenu === 'ClinicMenu') {
+      return ClinicSubMenu
+    } else if (activeMenu === 'FeedbackMenu') {
+      return FeedbackMenu
+    } else if (activeMenu === 'HelpMenu') {
+      return HelpMenu
+    } else if (activeMenu === 'LangMenu') {
+      return LangMenu
+    }
+  }
+
   return (
-    <AntDropDown overlay={menu} placement="bottomRight" arrow trigger={['click']}>
-      <Button>Avatar</Button>
-    </AntDropDown>
+    <div style={{ margin: '15px' }}>
+      <Popover
+        content={avatarMenu}
+        trigger="click"
+        placement="bottomRight"
+        overlayClassName="avatar-popover"
+      >
+        <Button>Avatar</Button>
+      </Popover>
+    </div>
   )
 }
 
