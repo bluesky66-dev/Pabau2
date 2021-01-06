cd7520e43ddd12c3792e75a12581192866ef7327
James Colton <james.c@pabau.com>
2021-01-06 08:52:15 +0000

    new cicd

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react'
 import { Layout } from '@pabau/ui'
 import { version } from '../../../package.json'
 
+// test
+
 const Index: FC = () => {
   return <Layout pageTitle="Welcome">Version {version}</Layout>
 }
