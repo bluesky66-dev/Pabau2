1d5bba05af08ebbf9cc95d985f2e826b09a5a430
James Colton <james.c@pabau.com>
2021-01-06 05:46:35 +0000

    new cicd

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,22 +2,25 @@ const withAntdLess = require('next-plugin-antd-less');
 const withImages = require('next-images');
 const withNx = require('@nrwl/next/plugins/with-nx');
 
-module.exports = withImages({
-  fileExtensions: ["jpg", "jpeg", "png", "gif", "ico", "webp", "jp2", "avif"],
-  inlineImageLimit: 10000,
-  ...withAntdLess({
-    importLoaders: 3,
-    cssLoaderOptions: {
-      sourceMap: true,
-      esModule: true,
-      modules: {
-        exportLocalsConvention: 'camelCase',
-        // exportOnlyLocals: false,
-        mode: 'local',
+module.exports = {
+  trailingSlash: true,
+  ...withImages({
+    fileExtensions: ["jpg", "jpeg", "png", "gif", "ico", "webp", "jp2", "avif"],
+    inlineImageLimit: 10000,
+    ...withAntdLess({
+      importLoaders: 3,
+      cssLoaderOptions: {
+        sourceMap: true,
+        esModule: true,
+        modules: {
+          exportLocalsConvention: 'camelCase',
+          // exportOnlyLocals: false,
+          mode: 'local',
+        },
       },
-    },
-    ...withNx({
-      cssModules: false,
+      ...withNx({
+        cssModules: false,
+      })
     })
   })
-});
+};
