13c46f1ab76fac7fbb0142b937e65a4bc98a05b1
James Colton <james.c@pabau.com>
2021-01-07 03:26:12 +0000

    cal page fix trailing slash deploy script fix ant-btn uppercase

--- apps/web/pages/cal/index.tsx
+++ apps/web/pages/cal/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 import { Layout } from '@pabau/ui'
 
 const Index: FC = () => {
-  return <Layout pageTitle="Cal">cal to go here.</Layout>
+  return <Layout pageTitle="Cal">cal to go here..</Layout>
 }
 
 export default Index
