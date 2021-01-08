dce9eae03a47d25cdeb74c3a91e23388671843cf
James Colton <james.c@pabau.com>
2021-01-08 06:59:21 +0000

    add grid concept to homepage

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,12 +1,20 @@
-import React, { FC } from 'react'
-import { Layout } from '@pabau/ui'
+import React, { FC, useState } from 'react'
+import { Button, Layout } from '@pabau/ui'
 import { version } from '../../../package.json'
 import useTranslation from '../hooks/useTranslation'
+import Grid from '../components/Grid'
 
 const Index: FC = () => {
   const { t } = useTranslation()
+  const [showGrid, setShowGrid] = useState(false)
 
-  return <Layout pageTitle={t('common', 'index.title')}>Version {version}</Layout>
+  return (
+    <Layout pageTitle={t('common', 'index.title')}>
+      {!showGrid && <Button onClick={() => setShowGrid(true)}>Edit</Button>}
+      {showGrid && <Grid />}
+      Version {version}
+    </Layout>
+  )
 }
 
 export default Index
