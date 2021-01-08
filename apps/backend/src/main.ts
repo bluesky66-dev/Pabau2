fa07ecf8627f750ad10b1769e94753c49a45a569
James Colton <james.c@pabau.com>
2021-01-08 11:44:33 +0000

    fix backend deploy

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,21 +1,38 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+// /**
+//  * This is not a production server yet!
+//  * This is only a minimal backend to get started.
+//  */
+//
+// import { Logger } from '@nestjs/common'
+// import { NestFactory } from '@nestjs/core'
+//
+// import { AppModule } from './app/app.module'
+//
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule)
+//   // const globalPrefix = 'api'
+//   // app.setGlobalPrefix(globalPrefix)
+//   const port = process.env.PORT || 3333
+//   await app.listen(port, () => {
+//     Logger.log('Listening at http://localhost:' + port + '/')
+//   })
+// }
+//
+// bootstrap()
 
-import { Logger } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app/app.module'
+import { NestFactory } from '@nestjs/core'
+import { AppController } from './app/app.controller'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  // const globalPrefix = 'api'
-  // app.setGlobalPrefix(globalPrefix)
-  const port = process.env.PORT || 3333
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/')
+function invokeNest(req, res) {
+  NestFactory.create(AppModule).then(e => {
+    const app2 = e.get(AppController)
+    res.json(app2.getData())
   })
 }
 
-bootstrap()
+
+export default function(req, res) {
+  invokeNest(req, res)
+}
