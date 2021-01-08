8da3f3be2b1d9050a6320617c54e019da5776379
James Colton <james.c@pabau.com>
2021-01-08 12:42:02 +0000

    fix backend deploy

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import logger from '../lib/logger'
 import { yup } from '@pabau/yup'
+import { version } from '../../../../package.json'
 
 @Injectable()
 export class AppService {
   getData(data?): { message: string } {
     if (data) logger(data);
-    return { message: 'Welcome to backend! This should say yup --> ' + yup() + JSON.stringify(data) }
+    return { message: `Welcome to backend v${version}! This should say yup --> ` + yup() + JSON.stringify(data) }
   }
 
 
