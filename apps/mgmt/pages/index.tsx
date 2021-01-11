0b1d9fa65f6cee19d67c73761c0d59f72e9b6757
James Colton <james.c@pabau.com>
2021-01-11 00:55:54 +0000

    cicd stuff

--- apps/mgmt/pages/index.tsx
+++ apps/mgmt/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { yup } from '@pabau/yup'
+import { Button, ButtonTypes } from '@pabau/ui'
 
 
 export function Index() {
@@ -10,6 +11,7 @@ export function Index() {
    */
   return (
     <div>
+      <Button type={ButtonTypes.primary}>mgmt</Button>
       <h2>Resources &amp; Tools</h2>
       <p>Thank you for using and showing some ♥ for Nx.</p>
       <div className="flex github-star-container">
