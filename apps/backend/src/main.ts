74586bc58fbf881ce702f64e0acc2d9aca05a986
James Colton <james.c@pabau.com>
2021-01-11 16:03:09 +0000

    vercel dev

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,34 +1,12 @@
-// /**
-//  * This is not a production server yet!
-//  * This is only a minimal backend to get started.
-//  */
-//
-// import { Logger } from '@nestjs/common'
-// import { NestFactory } from '@nestjs/core'
-//
-// import { AppModule } from './app/app.module'
-//
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule)
-//   // const globalPrefix = 'api'
-//   // app.setGlobalPrefix(globalPrefix)
-//   const port = process.env.PORT || 3333
-//   await app.listen(port, () => {
-//     Logger.log('Listening at http://localhost:' + port + '/')
-//   })
-// }
-//
-// bootstrap()
-
 import { NowRequest, NowResponse } from '@vercel/node'
 import { AppModule } from './app/app.module'
 import { NestFactory } from '@nestjs/core'
 import { AppController } from './app/app.controller'
 
 function invokeNest(req: NowRequest, res: NowResponse) {
-  NestFactory.create(AppModule).then(e => {
+  NestFactory.create(AppModule).then(async e => {
     const app2 = e.get(AppController)
-    res.json(app2.getData())
+    res.json(await app2.getData())
   })
 }
 
