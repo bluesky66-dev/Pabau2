42a971489da8c7db61fefaf597cb8d369eebda07
James Colton <james.c@pabau.com>
2021-01-11 23:31:02 +0000

    backend now supports dev mode via .run/

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,6 +2,22 @@ import { NowRequest, NowResponse } from '@vercel/node'
 import { AppModule } from './app/app.module'
 import { NestFactory } from '@nestjs/core'
 import { AppController } from './app/app.controller'
+import { Logger } from '@nestjs/common'
+
+async function bootstrapDevServer() {
+  const app = await NestFactory.create(AppModule)
+  // const globalPrefix = 'api'
+  // app.setGlobalPrefix(globalPrefix)
+  const port = process.env.PORT || 3333
+  await app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port + '/')
+  })
+}
+
+if (!process.env.VERCEL) {
+  console.log("STARTING DEV SERVER")
+  bootstrapDevServer()
+}
 
 function invokeNest(req: NowRequest, res: NowResponse) {
   NestFactory.create(AppModule).then(async e => {
