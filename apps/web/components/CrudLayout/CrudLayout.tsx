ef6a5102f6def30fddab7e358803b31d2231369e
James Colton <james.c@pabau.com>
2021-01-13 19:43:41 +0000

    add footer

--- apps/web/components/CrudLayout/CrudLayout.tsx
+++ apps/web/components/CrudLayout/CrudLayout.tsx
@@ -3,6 +3,7 @@ import { DocumentNode } from '@apollo/client'
 import CrudTable from '../CrudTable'
 import pluralize from 'pluralize'
 import Layout from '../Layout/Layout'
+import { Footer } from '@pabau/ui'
 
 interface P {
   schema: Schema
@@ -15,6 +16,7 @@ const CrudLayout: FC<P> = ({ ...props }) => (
   <Layout>
     <h1>{pluralize(props.schema.full || props.schema.short)}</h1>
     <CrudTable {...props} />
+    <Footer />
   </Layout>
 )
 
