e64ad4295681709c797393b51f30b1139313a7b2
James Colton <james.c@pabau.com>
2021-01-14 20:52:07 +0000

    graphql-let and other generators

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -21,6 +21,27 @@ module.exports = {
       },
       ...withNx({
         cssModules: false,
+        webpack(config, options) {
+          config.module.rules.push({
+            test: /\.graphql$/,
+            exclude: /node_modules/,
+            use: [options.defaultLoaders.babel, { loader: 'graphql-let/loader' }],
+          })
+
+          config.module.rules.push({
+            test: /\.graphqls$/,
+            exclude: /node_modules/,
+            use: ['graphql-let/schema/loader'],
+          })
+
+          config.module.rules.push({
+            test: /\.ya?ml$/,
+            type: 'json',
+            use: 'yaml-loader',
+          })
+
+          return config
+        },
       })
     })
   })
--- apps/web/types/global.d.ts
+++ apps/web/types/global.d.ts
@@ -1,1 +1,5 @@
 declare module '*.png'
+declare module '*.graphqls' {
+  import { DocumentNode } from 'graphql'
+  export default typeof DocumentNode
+}
