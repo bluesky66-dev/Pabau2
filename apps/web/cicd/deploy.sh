6f7c9a2a5d2b34ce6faef7d4a9039e7db72a098e
James Colton <james.c@pabau.com>
2021-01-16 17:06:44 +0000

    Merged in cicd_deploy_on_pr (pull request #17)
    cicd: deployment on PR
    
    * cicd: deployment on PR
    
    * cicd: deployment on PR
    
    * cicd: deployment on PR
    
    * cicd: deployment on PR
    
    * cicd: deployment on PR
    
    * cicd: deployment on PR

--- apps/web/cicd/deploy.sh
+++ apps/web/cicd/deploy.sh
@@ -1,23 +1,45 @@
 #!/usr/bin/env bash
 set -e
 
+echo "GIT_USER: ${GIT_USER}"
+echo "GIT_USER_NAME: ${GIT_USER_NAME}"
+
 echo "DEBUG: pwd=$(pwd)"
-APP_NAME="$(basename "$(dirname "$(cd "$(dirname "${0}")"; pwd)")")"
+APP_NAME="$(basename "$(dirname "$(
+  cd "$(dirname "${0}")"
+  pwd
+)")")"
 echo "DEBUG: app_name=${APP_NAME}"
 
 yarn run nx run "${APP_NAME}:export"
 cp "apps/${APP_NAME}/vercel.json" "dist/apps/${APP_NAME}/exported/"
 cd "dist/apps/${APP_NAME}/exported"
 
-OUTPUT=$(vercel -c -C --token "${VERCEL_TOKEN}" -A ./vercel.json)
-echo "errorlevel: $?"
-
-echo "Output from vercel:"
-echo "${OUTPUT}"
-echo "--"
-
-LAST_LINE=$(echo "${OUTPUT}" | tail -n1)
-echo "last line: ${LAST_LINE}"
-
 echo "commit hash: ${BITBUCKET_COMMIT}"
-vercel --token "${VERCEL_TOKEN}" alias "${LAST_LINE}" prelive-crm.new.pabau.com
+
+echo "BITBUCKET_PR_ID: ${BITBUCKET_PR_ID}"
+
+if [ -n "${BITBUCKET_PR_ID}" ]; then
+  OUTPUT=$(vercel -c -C --token "${VERCEL_TOKEN}" -A ./vercel.json --prod)
+  echo "errorlevel: $?"
+  echo "Output from vercel:"
+  echo "${OUTPUT}"
+  echo "--"
+  LAST_LINE=$(echo "${OUTPUT}" | tail -n1)
+  echo "last line: ${LAST_LINE}"
+  curl -X POST -H "Content-Type: application/json" \
+    -d '{"channel":"#pabau-2-dev","text":"'"${APP_NAME}"' for production deployed to '"${LAST_LINE}"'"}' \
+    "${SLACK_HOOK_URL}"
+else
+  OUTPUT=$(vercel -c -C --token "${VERCEL_TOKEN}" -A ./vercel.json)
+  echo "errorlevel: $?"
+  echo "Output from vercel:"
+  echo "${OUTPUT}"
+  echo "--"
+  LAST_LINE=$(echo "${OUTPUT}" | tail -n1)
+  echo "last line: ${LAST_LINE}"
+  curl -X POST -H "Content-Type: application/json" \
+    -d '{"channel":"#pabau-2-dev","text":"'"${APP_NAME}"' for PR ID '"${BITBUCKET_PR_ID}"' deployed to '"${LAST_LINE}"'"}' \
+    "${SLACK_HOOK_URL}"
+  #vercel --token "${VERCEL_TOKEN}" alias "${LAST_LINE}" "crm-pr-${BITBUCKET_PR_ID}.new.pabau.com"
+fi
