bf0812e9f440f0cd9a7ed4456d7fd4ee8bd96d24
James Colton <james.c@pabau.com>
2021-01-17 15:54:24 +0000

    cicd: template .json for slack message

--- apps/web/cicd/deploy.sh
+++ apps/web/cicd/deploy.sh
@@ -28,7 +28,9 @@ if [ -z "${BITBUCKET_PR_ID}" ]; then
   echo "last line: ${LAST_LINE}"
 
   export message_body="*New Version Staged for Production* - ${APP_NAME} v${PACKAGE_JSON_VERSION}\n\n${LAST_LINE}\n\n${LAST_COMMIT_LOG}"
-  jq --arg var "${message_body}" '.blocks[0].text.text = $var' tools/cicd/slack_notification.json | curl -0 "${SLACK_HOOK_URL}" \
+  {
+    jq --arg var "${message_body}" '.blocks[0].text.text = $var' <tools/cicd/slack_notification.json
+  } | curl -0 "${SLACK_HOOK_URL}" \
     -H "Expect:" \
     -H 'Content-Type: application/json; charset=utf-8' \
     --data-binary @-
@@ -45,8 +47,6 @@ else
   curl -X POST -H "Content-Type: application/json" \
     -d '{"channel":"#pabau-2-dev","text":"'"${APP_NAME}"' for PR ID '"${BITBUCKET_PR_ID}"' https://bitbucket.org/pabau/monorepo/pull-requests/'"${BITBUCKET_PR_ID}"' by '"${PR_AUTHOR}"' deployed to '"${LAST_LINE}"'"}' \
     "${SLACK_HOOK_URL}"
-
-
 
   #vercel --token "${VERCEL_TOKEN}" alias "${LAST_LINE}" "crm-pr-${BITBUCKET_PR_ID}.new.pabau.com"
 fi
