1bac7251d66c6602efec3f9cba5e0df8ecce9a64
Abdul Basit <zedongdingaura@gmail.com>
2021-01-20 02:39:05 +0800

    Fixed bugs on Notification Banner

--- libs/ui/src/lib/notificationBanner/NotificationBanner.tsx
+++ libs/ui/src/lib/notificationBanner/NotificationBanner.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Button} from '../../'
+import { Button } from '../button/button' 
 import styles from './NotificationBanner.module.less'
 import { Row, Col } from 'antd'
 
@@ -13,15 +13,15 @@ export interface NotificationBannerProps {}
 export function NotificationBanner(props: NotificationBannerProps) {
   return (
     <div className={styles.notificationBody}>
-      <Row style={{position: 'relative'}}>
+      <Row style={styles.rowPosition}>
         <Col md={16} sm={12}>
           <p className={styles.title1}>Enable online payment</p>
           <p className={styles.title2}>
             Activate payments with Fresha to benefit from tip collection during and after sale and get access to no show protection, payment terminals, safe online payments and many more.
           </p>
           <Button className={styles.btnPayment}
-            onClick={() => {}}
-            size="medium"
+            // onClick={() => {}}
+            size="middle"
             type="link"
           >
             Enable Payments
