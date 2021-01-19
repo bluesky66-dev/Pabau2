879ca8546382997a092d35282f5aa2a981cbfd01
Abdul Basit <zedongdingaura@gmail.com>
2021-01-20 02:03:13 +0800

    security - score (alpha)

--- libs/ui/src/lib/security/Security.stories.tsx
+++ libs/ui/src/lib/security/Security.stories.tsx
@@ -3,13 +3,22 @@ import React from 'react'
 import SecurityScore from './SecurityScore'
 import SecurityTools from './SecurityTools'
 
+
+
 export default {
   component: SecurityScore,
   title: 'Security',
+  args: { percent: 30},
+  argTypes: {
+    percent: { control: { type: 'number' } },
+  },
 }
 
-export const Score = () => {
-  return  <div style={{ background: '#f0f2f5', padding: '24px', height: '96vh' }}><SecurityScore /></div>
+const SecurityStory = ({ ...args }) => <SecurityScore {...args}></SecurityScore>
+
+export const Score = SecurityStory.bind({})
+Score.args = {
+  percent: 30,
 }
 export const Tools = () => {
   return (
--- libs/ui/src/lib/security/SecurityScore.tsx
+++ libs/ui/src/lib/security/SecurityScore.tsx
@@ -4,20 +4,34 @@ import styles from './Security.module.less'
 import { Row, Col, Progress } from 'antd'
 import {Button} from '../../lib/button/button'
 
-export interface SecurityProps {}
+export interface SecurityScoreProps {
+  percent: number
+}
 
-export function SecurityScore(props: SecurityProps) {
+export function SecurityScore(props: SecurityScoreProps) {
+  const {percent = 0 } = props
+  
+  var progressColor;
+
+  if ( percent < 30 ) {
+    progressColor = "red"
+  } else if ( percent >= 30 && percent < 60) {
+    progressColor = "yellow"
+  } else {
+    progressColor = "green"
+  }
+
   return (
     <div className={styles.scoreBody}>
       <Row style={{position: 'relative'}}>
         <Col md={5} sm={5}>
           <p className={styles.scoreTitle}>Pabau Baseline Standart</p>
-          <p className={styles.scorePercent}>76% Good</p>
+          <p className={styles.scorePercent}> {percent || 0 }% Good</p>
           <p className={styles.scoreTitle1}>of the standart met</p>
         </Col>
         <Col md = { 19 } sm = { 19}>
           
-            <Progress className={styles.scoreProgress} percent={100} showInfo={false} />
+            <Progress className={styles.scoreProgress} percent={percent || 0} strokeColor={ progressColor } showInfo={false} />
             <Button className={styles.btnScore} onClick={() => {}} size="medium" type="link">How do we calculate this score?</Button>
         </Col>
       </Row>
