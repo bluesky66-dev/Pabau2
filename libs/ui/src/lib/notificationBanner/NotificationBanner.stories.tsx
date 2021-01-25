d1504a88b739d45ae6f64ba17b300031312b90ed
Abdul Basit <zedongdingaura@gmail.com>
2021-01-25 06:21:54 +0000

    added hide action for notification banner

--- libs/ui/src/lib/notificationBanner/NotificationBanner.stories.tsx
+++ libs/ui/src/lib/notificationBanner/NotificationBanner.stories.tsx
@@ -1,25 +1,28 @@
 /* eslint-disable */
-import React from 'react'
-import NotificationBanner from './NotificationBanner'
-import icon from '../../assets/images/notification.png'
+import React from "react";
+import NotificationBanner from "./NotificationBanner";
+import icon from "../../assets/images/notification.png";
 
 export default {
-  component: NotificationBanner,
-  title: 'NotificationBanner',
-  args: { title : 'Enable online payment', desc : 'Activate payments with Fresha to benefit from tip collection during and after sale and get access to no show protection, payment terminals, safe online payments and many more.'},
-  argTypes: {
-    title: { control: { type: 'text' } },
-    desc: { control: { type: 'text' } },
-    imgPath: { control: {type: 'text'}},
-  },
-}
+    component: NotificationBanner,
+    title: "NotificationBanner",
+    args: {
+        title: "Enable online payment",
+		desc: "Activate payments with Fresha to benefit from tip collection during and after sale and get access to no show protection, payment terminals, safe online payments and many more.",
+    },
+    argTypes: {
+        title: { control: { type: "text" } },
+        desc: { control: { type: "text" } },
+		imgPath: { control: { type: "text" } },
+		onClick: { action : 'clicked'},
+    },
+};
 
-const NotificationBannerStory = ({ ...args }) => <NotificationBanner {...args}></NotificationBanner>
+const NotificationBannerStory = ({ ...args }) => <NotificationBanner {...args}></NotificationBanner>;
 
-export const DefaultNotificationBanner = NotificationBannerStory.bind({})
+export const DefaultNotificationBanner = NotificationBannerStory.bind({});
 DefaultNotificationBanner.args = {
-  title: 'Enable online payment',
-  desc : 'Activate payments with Fresha to benefit from tip collection during and after sale and get access to no show protection, payment terminals, safe online payments and many more.',
-  imgPath : icon,
-}
-
+    title: "Enable online payment",
+    desc: "Activate payments with Fresha to benefit from tip collection during and after sale and get access to no show protection, payment terminals, safe online payments and many more.",
+    imgPath: icon,
+};
--- libs/ui/src/lib/notificationBanner/NotificationBanner.tsx
+++ libs/ui/src/lib/notificationBanner/NotificationBanner.tsx
@@ -1,44 +1,45 @@
 /* eslint-disable */
-import React from 'react'
+import React, { useState } from "react";
 
-import { Button } from '../button/button'
-import styles from './NotificationBanner.module.less'
-import { Row, Col } from 'antd'
-import iconClose from '../../assets/images/icon_close.svg'
+import { Button } from "../button/button";
+import styles from "./NotificationBanner.module.less";
+import { Row, Col } from "antd";
+import iconClose from "../../assets/images/icon_close.svg";
 interface NotificationBannerProps {
-  title?: string,
-  desc?: string,
-  imgPath?: string,
+    title?: string;
+    desc?: string;
+	imgPath?: string;
 }
 
 export function NotificationBanner(props: NotificationBannerProps) {
-  const { title = 'nil' } = props
-  const { desc = 'nil' } = props
-  const { imgPath = 'nil' } = props
-  return (
-    <div className={styles.notificationBody}>
-      <Row className={styles.rowPosition}>
-        <Col md={16} sm={12}>
-          <p className={styles.title1}>{title}</p>
-          <p className={styles.title2}>{desc}</p>
-          <Button
-            className={styles.btnPayment}
-            // onClick={() => {}}
-            size="middle"
-            type="link"
-          >
-            Enable Payments
-          </Button>
-        </Col>
-        <img className={styles.iconClose} src={iconClose} alt="" />
-        <img className={styles.backgroundImg} src={imgPath} alt="" />
-      </Row>
-    </div>
-  )
+    const { title = "", desc = "", imgPath = "" } = props;
+
+    const [ isHide , setHide] = useState(false)
+
+    return (
+        <div className={ isHide? styles.hideBlock : styles.notificationBody}>
+            <Row className={styles.rowPosition}>
+                <Col md={16} sm={12}>
+                    <p className={styles.title1}>{title}</p>
+                    <p className={styles.title2}>{desc}</p>
+                    <Button
+                        className={styles.btnPayment}
+                        // onClick={() => {}}
+                        size="middle"
+                        type="link"
+                    >
+                        Enable Payments
+                    </Button>
+                </Col>
+                <img className={styles.iconClose} src={iconClose} onClick={ () => setHide(true)} alt="" />
+                <img className={styles.backgroundImg} src={imgPath} alt="" />
+            </Row>
+        </div>
+    );
 }
 
 // const NotificationBanner: React.FunctionComponent<NotificationBannerProps> = (props) => (
-  
+
 // )
 
-export default NotificationBanner
+export default NotificationBanner;
