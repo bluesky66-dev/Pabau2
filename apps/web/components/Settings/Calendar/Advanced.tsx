88a967ba2f50a41fbe6eb13809b76e31a697895a
Amar Shah <shahamar538@gmail.com>
2021-01-25 19:32:37 +0530

    PAB2-313 related changes

--- apps/web/components/Settings/Calendar/Advanced.tsx
+++ apps/web/components/Settings/Calendar/Advanced.tsx
@@ -1,17 +1,82 @@
-import React from 'react'
+import React, { FC } from 'react'
 import styles from './calendar.module.less'
-import { Typography } from 'antd'
+import { Typography, Checkbox } from 'antd'
+import { QuestionCircleOutlined } from '@ant-design/icons'
 
 const { Title } = Typography
+interface AdvancedControlItems {
+  type: string
+  value: boolean
+}
 
-const Advanced = () => (
-  <div className={styles.calendarSettingsAdvanced}>
-    <div className={styles.settingContent}>
-      <Title className={styles.headerText}>Advanced</Title>
-      <span className={styles.description}>configure any further settings.</span>
+export const Advanced: FC = () => {
+  const advancedControls: AdvancedControlItems[] = [
+    {
+      type: 'Raise invoice when appointment is completed',
+      value: true,
+    },
+    {
+      type: 'Advanced calendar lockout',
+      value: false,
+    },
+    {
+      type: 'Disable booking from package',
+      value: true,
+    },
+    {
+      type: 'Disable filtering by service',
+      value: true,
+    },
+    {
+      type: 'Auto-complete',
+      value: true,
+    },
+    {
+      type: 'Ordering the employees by location',
+      value: true,
+    },
+    {
+      type: 'Hide client name on appointment arrival notification',
+      value: true,
+    },
+    {
+      type: "Allow service without contract's price",
+      value: true,
+    },
+    {
+      type: 'Hide/Show accounts',
+      value: true,
+    },
+    {
+      type: 'Group user columns by location',
+      value: true,
+    },
+    {
+      type: 'Revert to original room view',
+      value: true,
+    },
+  ]
+  return (
+    <div className={styles.calendarSettingsAdvanced}>
+      <div className={styles.settingContent}>
+        <Title className={styles.headerText}>Advanced</Title>
+        <span className={styles.description}>configure any further settings.</span>
+      </div>
+      <div className={styles.advancedControls}>
+        {advancedControls.map((advanced, index) => {
+          return (
+            <>
+              <Checkbox defaultChecked={advanced.value}>
+                <span className={styles.appointmentText}>{advanced.type}</span>
+              </Checkbox>
+              <QuestionCircleOutlined className={styles.advancedIcon} />
+              <br />
+            </>
+          )
+        })}
+      </div>
     </div>
-    <div className={styles.appearancesControls}>{}</div>
-  </div>
-)
+  )
+}
 
 export default Advanced
--- apps/web/components/Settings/Calendar/Appearance.tsx
+++ apps/web/components/Settings/Calendar/Appearance.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './calendar.module.less'
-import { Typography, Checkbox } from 'antd'
+import { Typography, Checkbox, Select } from 'antd'
 import { QuestionCircleOutlined } from '@ant-design/icons'
 
 const { Title } = Typography
@@ -27,6 +27,7 @@ const AppearanceGeneralItems: AppearanceGeneralItems[] = [
     value: true,
   },
 ]
+const options = [{ value: 'Small' }, { value: 'Medium' }, { value: 'Large' }]
 const Appearance = () => (
   <div className={styles.calendarSettingsAppearance}>
     <div className={styles.settingContent}>
@@ -36,20 +37,37 @@ const Appearance = () => (
         both the tooltip and the <br /> appointment card itself.
       </span>
     </div>
-    <div className={styles.appearancesControls}>
-      <b>General</b>
-      <span>Some description here.</span>
-      {AppearanceGeneralItems.map((general, index) => {
-        return (
-          <>
-            <Checkbox defaultChecked={general.value}>
-              <span className={styles.appointmentText}>{general.type}</span>
-            </Checkbox>
-            <QuestionCircleOutlined className={styles.appointmentIcon} />
-            <br />
-          </>
-        )
-      })}
+    <div className={styles.generalControls}>
+      <div className={styles.generalBlock}>
+        <Title className={styles.blockText} level={4}>
+          General
+        </Title>
+        {AppearanceGeneralItems.map((general, index) => {
+          return (
+            <>
+              <Checkbox defaultChecked={general.value}>
+                <span className={styles.appointmentText}>{general.type}</span>
+              </Checkbox>
+              <QuestionCircleOutlined className={styles.appointmentIcon} />
+              <br />
+            </>
+          )
+        })}
+      </div>
+    </div>
+    <div className={styles.fontControls}>
+      <div className={styles.fontBlock}>
+        <Title className={styles.blockText} level={4}>
+          Font size
+        </Title>
+        <Select
+          className={styles.fontSizeSelect}
+          style={{ width: '100%' }}
+          placeholder="Please select"
+          defaultValue={['Small']}
+          options={options}
+        />
+      </div>
     </div>
   </div>
 )
