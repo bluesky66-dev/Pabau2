1969ac17b236f46048500d7f26df30e76ade4ba2
sparth786 <sparth786@gmail.com>
2021-01-25 19:12:59 +0530

    Added Notifications Inner component

---
+++ libs/ui/src/lib/notificationMessages/NotificationMessages.tsx
@@ -0,0 +1,54 @@
+import React, { ReactNode } from 'react'
+import { Badge } from '../..'
+import styles from './NotificationMessages.module.less'
+
+export interface notificationDataType {
+  header: string
+  description: string
+  icon: ReactNode
+  smartDelivery: boolean
+  link: string
+}
+
+export interface NotificationMessagesProps {
+  notificationData?: notificationDataType[]
+  onClick?: (selectedLink: string) => void
+}
+
+export function NotificationMessages(props: NotificationMessagesProps): JSX.Element {
+  const handleClick = (notification) => {
+    props.onClick?.(notification)
+  }
+
+  return (
+    <div>
+      {props?.notificationData?.map((notification, index) => (
+        <div
+          className={styles.scoreBody}
+          key={index}
+          onClick={() => handleClick(notification.link)}
+        >
+          <div className={styles.container}>
+            <div className={styles.colStatusLabel}>
+              <span className={styles.iconSecuritytools}>{notification.icon}</span>
+            </div>
+            <div className={styles.containerCol}>
+              <span className={styles.p1}>{notification.header}</span>
+              <span className={styles.p2}>{notification.description}</span>
+            </div>
+            <div className={styles.stateLabel}>
+              <div className={styles.badgeLabel}>
+                <Badge />
+              </div>
+              <div className={styles.smartLabel}>
+                {notification.smartDelivery && <span> +Smart delivery</span>}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default NotificationMessages
