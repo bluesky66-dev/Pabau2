80bba4207a3d22cd490e088875f3fa0f95b94e8b
Rocky Wang <wang.mythaero@gmail.com>
2021-01-26 06:19:16 -0800

    Remove unnecessary comment

--- libs/ui/src/lib/time-input/TimeInput.tsx
+++ libs/ui/src/lib/time-input/TimeInput.tsx
@@ -3,7 +3,6 @@ import { TimePicker } from 'antd'
 import { TimePickerProps } from 'antd/lib/time-picker'
 import styles from './TimeInput.module.less'
 
-/* eslint-disable-next-line */
 interface TimeInputProps extends TimePickerProps {
   label?: string
 }
