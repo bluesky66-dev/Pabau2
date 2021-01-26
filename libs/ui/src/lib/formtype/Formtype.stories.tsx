2ae9b182e37d5c3e406a89cac2a452e76752b487
D.A. Zhen <dokam2050@gmail.com>
2021-01-25 20:36:45 -0500

    PAB2-427

--- libs/ui/src/lib/formtype/Formtype.stories.tsx
+++ libs/ui/src/lib/formtype/Formtype.stories.tsx
@@ -5,12 +5,6 @@ import FormtypePanel from './Formtype'
 export default {
   component: FormtypePanel,
   title: 'UI/Form Type',
-  // args: { title : 'Enable online payment', desc : 'Activate payments with Fresha to benefit from tip collection during and after sale and get access to no show protection, payment terminals, safe online payments and many more.'},
-  // argTypes: {
-  //   title: { control: { type: 'text' } },
-  //   desc: { control: { type: 'text' } },
-  //   imgPath: { control: {type: 'text'}},
-  // },
 }
 
 const FormtypeStory = ({ ...args }) => <FormtypePanel {...args}></FormtypePanel>
--- libs/ui/src/lib/formtype/Formtype.tsx
+++ libs/ui/src/lib/formtype/Formtype.tsx
@@ -1,5 +1,6 @@
+import { CheckCircleFilled } from '@ant-design/icons'
 import { Button, Col, Row, Tooltip } from 'antd'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import consentIcon from '../../assets/images/consent.png'
 import epaperIcon from '../../assets/images/epaper.png'
 import labFormIcon from '../../assets/images/lab-form.png'
@@ -14,6 +15,7 @@ const Formtype: FC<FormtypeProps> = () => {
   const formTypeInfos = [
     {
       index: 1,
+      name: 'medical_history',
       label: 'Medical History',
       desc:
         'A medical history form can be completed and updated multiple times, whilst retaining any previously completed information     ',
@@ -21,18 +23,21 @@ const Formtype: FC<FormtypeProps> = () => {
     },
     {
       index: 2,
+      name: 'consent',
       label: 'Consent',
       desc: 'A consent form requires a signature as part of the care pathway',
       icon: consentIcon,
     },
     {
       index: 3,
+      name: 'treatment_form',
       label: 'Treatment Form',
       desc: 'A treatment form is usually completed at the end of a pathway',
       icon: treatmentIcon,
     },
     {
       index: 4,
+      name: 'epaper',
       label: 'ePaper',
       desc:
         'ePaper allows you to either scan in a document or upload a PDF to draw directly onto the screen using a stylus or apple pencil',
@@ -40,27 +45,30 @@ const Formtype: FC<FormtypeProps> = () => {
     },
     {
       index: 5,
+      name: 'presciption',
       label: 'Presciption',
       desc: 'Presciption',
       icon: presciptionIcon,
     },
     {
       index: 6,
+      name: 'lab_form',
       label: 'Lab Form',
       desc: 'Lab Form',
       icon: labFormIcon,
     },
   ]
-
+  const [selected, setSelected] = useState('')
   return (
     <Row>
-      {formTypeInfos.map(({ label, desc, icon, index }) => (
+      {formTypeInfos.map(({ name, label, desc, icon, index }) => (
         <Col key={index} span={8}>
           <div className={styles.formTypeDiv}>
+            {selected === name && <CheckCircleFilled className={styles.formTypeChecked} />}
             <Row>
               <Tooltip placement="topLeft" title={desc}>
-                <Button className={styles.formTypeButton}>
-                  <img src={icon} alt="" />
+                <Button className={styles.formTypeButton} onClick={() => setSelected(name)}>
+                  <img className={styles.formTypeIcon} src={icon} alt="" />
                 </Button>
                 <p className={styles.formTypeLabel}>{label}</p>
               </Tooltip>
