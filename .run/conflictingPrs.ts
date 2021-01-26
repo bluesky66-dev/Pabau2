872bc375095c1fb397a6d2ea004a7da027d3efa4
James Colton <james.c@pabau.com>
2021-01-26 16:47:02 +0000

    [skip ci] devops: conflictingPrs script

--- .run/conflictingPrs.ts
+++ .run/conflictingPrs.ts
@@ -1,6 +1,7 @@
 import { execSync } from 'child_process'
 import {join} from 'path'
 console.log("Starting", process.cwd(), "in", __dirname)
+const child_process = require('child_process');
 import { Bitbucket } from 'bitbucket'
 import fetch from 'node-fetch'
 const env = require('dotenv').config({debug:true, path:join(__dirname, ".env")})
@@ -52,8 +53,36 @@ const bbConfig = {
       ...bbConfig,
     })
     .then((e) => e.data.values)
+  child_process.spawnSync(`git`, [`fetch`]);
   for (const e of bbList) {
-    console.log(e.source.commit.hash)
-    console.log(execSync(`git checkout ${e.source.commit.hash}`).toString())
+    //console.log(e.author.display_name)
+
+    const checkout = child_process.spawnSync(`git`, [`checkout`, `${e.source.commit.hash}`]);
+    //console.log("proc.status=", checkout.status);
+    if (checkout.status > 0) {
+      console.error("can't do git checkout, is your work tree clean?");
+      console.log(checkout.stderr.toString())
+      process.exit(1);
+    }
+
+    const merge = child_process.spawnSync(`git`, [`merge`, `origin/master`, `--no-commit`, '--no-ff']);
+    if (merge.status > 0) {
+      //console.log(`${e.author.display_name}: ${e.source.branch.name}`);
+      merge.stdout.toString().split('\n').forEach(e => {
+        if (e.substr(0, 8) === "CONFLICT") {
+          //console.log(e)
+        }
+      })
+      //console.log();
+    } else {
+      //console.log("OK"); //, merge.stdout.toString(), merge.stderr.toString())
+      const approvals = e.participants?.reduce((a,c)=>a+(c.approved ? 1 : 0), 0)
+      console.log(`${e.author.display_name}: ${e.source.branch.name}: ${approvals}: ${e.links.html.href}`);
+    }
+
+    child_process.spawnSync(`git`, [`reset`, `--hard`]);
+
   }
+
+  child_process.spawnSync(`git`, [`checkout`, `master`])
 })();
