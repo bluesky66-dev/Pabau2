202ec1c07e736642b4a3c69add4aff7fe4e8c5eb
--global <--global>
2021-01-27 21:24:36 +0530

    fixed build

--- apps/web/pages/setup/webinar/index.jsx
+++ apps/web/pages/setup/webinar/index.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
-import { data } from './data';
 import { Webinar, Button } from '@pabau/ui'
 import styles from '../Setup.module.less'
+
+import backgroundImage from '../../../assets/images/user.png'
+
+const data = [
+    {
+        title: 'Stock Management',
+        name: 'Alexander Turner',
+        time: 'Tuesday, 15 10:00 AM',
+        timeLeft: '18:28',
+        backgroundImage,
+        isJoin: true,
+    },
+    {
+        title: 'Advanced Financials',
+        name: 'Alexander Turner',
+        time: 'Tuesday, 15 10:00 AM',
+        timeLeft: '18:28',
+        backgroundImage,
+    },
+    {
+        title: 'Basic Start-Up Training',
+        name: 'Alexander Turner',
+        time: 'Tuesday, 15 10:00 AM',
+        timeLeft: '18:28',
+        backgroundImage,
+    },
+    {
+        title: 'Advanced Financials',
+        name: 'Alexander Turner',
+        time: 'Tuesday, 15 10:00 AM',
+        timeLeft: '18:28',
+        backgroundImage,
+    },
+    {
+        title: 'Basic Start-Up Training',
+        name: 'Alexander Turner',
+        time: 'Tuesday, 15 10:00 AM',
+        timeLeft: '18:28',
+        backgroundImage,
+    },
+    {
+        title: 'Advanced Financials',
+        name: 'Alexander Turner',
+        time: 'Tuesday, 15 10:00 AM',
+        timeLeft: '18:28',
+        backgroundImage,
+    },
+    {
+        title: 'Basic Start-Up Training',
+        name: 'Alexander Turner',
+        time: 'Tuesday, 15 10:00 AM',
+        timeLeft: '18:28',
+        backgroundImage,
+    }
+]
 
 const WebinarCard = () => {
     return (
