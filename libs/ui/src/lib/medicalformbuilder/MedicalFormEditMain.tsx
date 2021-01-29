9279a744b971ebec20beda6fc0bdb5c947902b0f
D.A. Zhen <dokam2050@gmail.com>
2021-01-28 22:41:05 -0500

    PAB2-426

---
+++ libs/ui/src/lib/medicalformbuilder/MedicalFormEditMain.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react'
+import backImg from '../../assets/images/medicalform_back.svg'
+import backgroundImg from '../../assets/images/medicalform_builder.svg'
+import styles from './MedicalFormBuilder.module.less'
+
+const MedicalFormEditMain: FC = () => {
+  return (
+    <div className={styles.medicalFormEditMainPanel}>
+      <img src={backgroundImg} />
+      <h1>Start building your form with components</h1>
+      <span>Drag and drop components from the left menu here</span>
+      <div className={styles.medicalFormEditDesc}>
+        <img src={backImg} />
+        <span>Place your first component here</span>
+      </div>
+    </div>
+  )
+}
+
+export default MedicalFormEditMain
