2abfb49c12266982391fbf8637e076398196a40a
Rocky Wang <wang.mythaero@gmail.com>
2021-01-28 19:02:27 -0800

    Update PhoneNumberInput - validate phone number

--- libs/ui/src/lib/phone-number-input/PhoneNumberInput.tsx
+++ libs/ui/src/lib/phone-number-input/PhoneNumberInput.tsx
@@ -1,8 +1,11 @@
 import React, { FC, useState } from 'react'
 import PhoneInput from 'react-phone-input-2'
+import { PhoneNumberUtil } from 'google-libphonenumber'
+import ClassNames from 'classnames'
 import styles from './PhoneNumberInput.module.less'
 import 'react-phone-input-2/lib/style.css'
 
+const phoneUtil = PhoneNumberUtil.getInstance()
 export interface PhoneNumberInputProps {
   countryCode?: string
   onChange(val): void
@@ -13,21 +16,42 @@ export const PhoneNumberInput: FC<PhoneNumberInputProps> = ({
   onChange,
 }) => {
   const [phoneNumber, setPhoneNumber] = useState('')
-  const handleChangeInput = (val) => {
+  const [valid, setValid] = useState(true)
+  const handleChangeInput = (val, country) => {
+    try {
+      const isValid = phoneUtil.isValidNumberForRegion(
+        phoneUtil.parse(val, country.countryCode.toUpperCase()),
+        country.countryCode.toUpperCase()
+      )
+      setValid(isValid)
+    } catch {
+      setValid(false)
+    }
     setPhoneNumber(val)
     onChange(`${val}`)
   }
 
   return (
     <div className={styles.phoneNumberInputContainer}>
-      <p>Phone Number</p>
-      <div className={styles.phoneNumberInput}>
+      <p className={styles.phoneNumberLabel}>Phone Number</p>
+      <div
+        className={
+          valid
+            ? styles.phoneNumberInput
+            : ClassNames(styles.phoneNumberInput, styles.redBorder)
+        }
+      >
         <PhoneInput
           country={countryCode.toLowerCase()}
           value={phoneNumber}
-          onChange={(val) => handleChangeInput(val)}
+          onChange={(value, country) => handleChangeInput(value, country)}
         />
       </div>
+      {!valid && (
+        <p className={styles.phoneNumberValidMsg}>
+          Please enter a valid phone number
+        </p>
+      )}
     </div>
   )
 }
