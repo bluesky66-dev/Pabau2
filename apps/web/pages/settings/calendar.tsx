9e05b7ec4f9e294b4e67ee5231ef983bac5daa89
jemink <kothari.jemin.dev@gmail.com>
2021-01-29 21:05:17 +0530

    wired calendar component on setting page

--- apps/web/pages/settings/calendar.tsx
+++ apps/web/pages/settings/calendar.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
-import { SettingsMenu } from '@pabau/ui'
+import { SettingsMenu, Button } from '@pabau/ui'
+import { Card } from 'antd'
+import CommonHeader from '../setup/CommonHeader'
+import { Layout } from '@pabau/ui'
 import Configuration from '../../components/Settings/Calendar/Configuration'
 import AppointmentSettings from '../../components/Settings/Calendar/AppointmentSettings'
 import Appearance from '../../components/Settings/Calendar/Appearance'
 import Advanced from '../../components/Settings/Calendar/Advanced'
+import styles from './calendar.module.less'
+import { RightOutlined } from '@ant-design/icons'
 
 /* eslint-disable-next-line */
-export interface CalendarProps {}
+export interface CalendarProps { }
 
 export function Calendar(props: CalendarProps) {
   const generalSettingsMenu1 = [
@@ -28,10 +33,29 @@ export function Calendar(props: CalendarProps) {
       component: <Advanced />,
     },
   ]
+
   return (
     <div>
-      <h1>Calendar settings</h1>
-      <SettingsMenu items={generalSettingsMenu1} />
+      <CommonHeader />
+      <Layout>
+        <Card className={styles.calendarCard}>
+          <div className={styles.mainTabWrapper}>
+            <div className={styles.titleWrapper}>
+              <p className={styles.tabWrap}>
+                {'Setup'}
+                <span>
+                  <RightOutlined /> Marketing Source
+                </span>
+              </p>
+              <h1>Calendar settings</h1>
+            </div>
+            <div className={styles.saveBtn}>
+              <Button type="primary">Save Changes</Button>
+            </div>
+          </div>
+          <SettingsMenu items={generalSettingsMenu1} />
+        </Card>
+      </Layout>
     </div>
   )
 }
