78b94b3daab24d96c26819c017bb65ea2a670900
Rocky Wang <wang.mythaero@gmail.com>
2021-01-28 16:28:23 -0800

    Update PhoneNumberInput using react-phone-input-2

--- libs/ui/src/lib/phone-number-input/PhoneNumberInput.tsx
+++ libs/ui/src/lib/phone-number-input/PhoneNumberInput.tsx
@@ -1,10 +1,7 @@
-import React, { FC, useState, useEffect } from 'react'
-import { Input, Dropdown, Menu } from 'antd'
-import { DownOutlined } from '@ant-design/icons'
-import PhoneNumberAreaCodes from './PhoneNumberAreaCodes'
-import PhoneInput from 'react-phone-number-input/input'
-import { formatPhoneNumber } from 'react-phone-number-input'
+import React, { FC, useState } from 'react'
+import PhoneInput from 'react-phone-input-2'
 import styles from './PhoneNumberInput.module.less'
+import 'react-phone-input-2/lib/style.css'
 
 export interface PhoneNumberInputProps {
   countryCode?: string
@@ -15,60 +12,20 @@ export const PhoneNumberInput: FC<PhoneNumberInputProps> = ({
   countryCode = 'GB',
   onChange,
 }) => {
-  const [phoneCode, setPhoneCode] = useState('+44')
   const [phoneNumber, setPhoneNumber] = useState('')
-  const [number, setNumber] = useState('')
-  const handleMenuClick = (e) => {
-    setPhoneCode(`+${PhoneNumberAreaCodes[e.key][1]}`)
+  const handleChangeInput = (val) => {
+    setPhoneNumber(val)
+    onChange(`${val}`)
   }
-  const handleChangePhoneNumber = (val) => {
-    const value = formatPhoneNumber(val)
-    setNumber(value)
-  }
-  const handleChangeInput = (e) => {
-    const { value } = e.target
-    const pattern = new RegExp(/^\d*$/i)
-    if (pattern.test(value)) {
-      setPhoneNumber(value)
-      onChange(`${phoneCode}${value}`)
-    }
-  }
-  const menu = (
-    <Menu className={styles.menuContainer} onClick={(e) => handleMenuClick(e)}>
-      {PhoneNumberAreaCodes.map((code, index) => (
-        <Menu.Item key={index}>{`${code[2]} (+${code[1]})`}</Menu.Item>
-      ))}
-    </Menu>
-  )
-  const selectBefore = (
-    <Dropdown overlay={menu} trigger={['click']}>
-      <div className={styles.phoneCodeContainer}>
-        <p>{phoneCode}</p>
-        <DownOutlined />
-      </div>
-    </Dropdown>
-  )
-
-  useEffect(() => {
-    const phoneNumberCode = PhoneNumberAreaCodes.find(
-      (code) => code[2] === countryCode
-    )
-    if (phoneNumberCode) setPhoneCode(`+${phoneNumberCode[1]}`)
-  }, [countryCode])
 
   return (
     <div className={styles.phoneNumberInputContainer}>
       <p>Phone Number</p>
       <div className={styles.phoneNumberInput}>
-        <Input
-          addonBefore={selectBefore}
-          value={phoneNumber}
-          onChange={(e) => handleChangeInput(e)}
-        />
         <PhoneInput
-          country={countryCode}
-          value={number}
-          onChange={(val) => handleChangePhoneNumber(val)}
+          country={countryCode.toLowerCase()}
+          value={phoneNumber}
+          onChange={(val) => handleChangeInput(val)}
         />
       </div>
     </div>
