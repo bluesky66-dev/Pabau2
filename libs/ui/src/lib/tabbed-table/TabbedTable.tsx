d562dfc7aa5cc8f178a8f2adaaddd78a23eb48ae
Rocky Wang <wang.mythaero@gmail.com>
2021-02-03 10:08:37 -0800

    Updates for TabbedTable component

--- libs/ui/src/lib/tabbed-table/TabbedTable.tsx
+++ libs/ui/src/lib/tabbed-table/TabbedTable.tsx
@@ -1,29 +1,17 @@
-import React, { FC, useState, useEffect } from 'react'
-import { TabMenu, Table, TableType } from '@pabau/ui'
+import React, { FC, ReactNode } from 'react'
+import { TabMenu } from '@pabau/ui'
 import styles from './TabbedTable.module.less'
-
-interface TableData {
-  [key: string]: TableType
-}
 export interface TabbedTableProps {
   tabItems: Array<string>
-  tableData: TableData
+  children: ReactNode[]
 }
 
-export const TabbedTable: FC<TabbedTableProps> = ({ tabItems, tableData }) => {
-  const [valid, setValid] = useState(false)
-  useEffect(() => {
-    const broken = tabItems.findIndex((item) => tableData[item] === undefined)
-    setValid(broken === -1)
-  }, [tabItems, tableData])
+export const TabbedTable: FC<TabbedTableProps> = ({ tabItems, children }) => {
   return (
     <div className={styles.tabbedTableContainer}>
-      {valid && (
-        <TabMenu menuItems={tabItems || []} tabPosition="top">
-          {tabItems &&
-            tabItems.map((item) => <Table key={item} {...tableData[item]} />)}
-        </TabMenu>
-      )}
+      <TabMenu menuItems={tabItems || []} tabPosition="top">
+        {children?.map((child) => child)}
+      </TabMenu>
     </div>
   )
 }
