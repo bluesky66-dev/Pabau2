c9441cedd25aa8635af644bc25f699f424063786
Rocky Wang <wang.mythaero@gmail.com>
2021-02-04 06:22:42 -0800

    Update spec for Formtype

--- libs/ui/src/lib/formtype/Formtype.spec.tsx
+++ libs/ui/src/lib/formtype/Formtype.spec.tsx
@@ -3,9 +3,20 @@ import { render } from '@testing-library/react'
 
 import Formtype from './Formtype'
 
+const setting = {
+  medicalHistory: false,
+  consent: false,
+  treatmentForm: false,
+  epaper: false,
+  presciption: false,
+  labForm: false,
+}
+
 describe('Formtype', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Formtype />)
+    const { baseElement } = render(
+      <Formtype setting={setting} onChangeSetting={(val) => val} />
+    )
     expect(baseElement).toBeTruthy()
   })
 })
