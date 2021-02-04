15e627266b0a49222036cd584f80d82e082b2e13
D.A. Zhen <dokam2050@gmail.com>
2021-02-03 22:17:27 -0500

    PAB2-555

--- libs/ui/src/lib/medicalform/MedicalForm.tsx
+++ libs/ui/src/lib/medicalform/MedicalForm.tsx
@@ -1,7 +1,37 @@
+import { Input } from 'antd'
 import React, { FC } from 'react'
+import headingIcon from '../../assets/images/medicalform_heading.svg'
+import styles from './MedicalForm.module.less'
+import MedicalFormBottom from './MedicalFormBottom'
+import MedicalFormTitle from './MedicalFormTitle'
 
 const MedicalForm: FC = () => {
-  return <div></div>
+  return (
+    <div className={styles.mainBody}>
+      <div className={styles.formItem}>
+        <div className={`${styles.formCaption} ${styles.formCommon}`}>
+          <span>component settings</span>
+        </div>
+      </div>
+      <div className={styles.formItem}>
+        <MedicalFormTitle
+          iconUrl={headingIcon}
+          bgcolor="#6383F1"
+          title="Heading"
+          desc="Create a heading for a section"
+        />
+      </div>
+      <div className={styles.formItem}>
+        <div className={`${styles.formQuestion} ${styles.formCommon}`}>
+          <p style={{ marginTop: '5px' }}>Question</p>
+          <Input placeholder="Enter your question" />
+        </div>
+      </div>
+      <div className={styles.formItem} style={{ borderBottom: 'none' }}>
+        <MedicalFormBottom needLeft={false} />
+      </div>
+    </div>
+  )
 }
 
 export default MedicalForm
