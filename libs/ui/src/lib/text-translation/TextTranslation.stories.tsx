c2f3308b7ab2966995e5a55fc71f5f2fba5da5d4
jemink <kothari.jemin.dev@gmail.com>
2021-02-04 17:03:59 +0530

    Added i18next addon in storybook

--- libs/ui/src/lib/text-translation/TextTranslation.stories.tsx
+++ libs/ui/src/lib/text-translation/TextTranslation.stories.tsx
@@ -1,34 +1,5 @@
 import React, { FC } from 'react'
 import TextTranslation from './TextTranslation'
-import { I18nextProvider } from 'react-i18next'
-import i18next from 'i18next'
-
-const en = {
-  welcome: {
-    title: 'Welcome',
-    desc: 'How are you?',
-  },
-}
-
-const de = {
-  welcome: {
-    title: 'Willkommen',
-    desc: 'Wie geht es dir?',
-  },
-}
-
-i18next.init({
-  interpolation: { escapeValue: false },
-  lng: 'en',
-  resources: {
-    en: {
-      common: en,
-    },
-    de: {
-      common: de,
-    },
-  },
-})
 
 export default {
   component: TextTranslation,
@@ -45,19 +16,9 @@ interface P {
   language?: string
 }
 
-const TextTranslationStory: FC<P> = ({ language }) => {
-  return (
-    <I18nextProvider i18n={i18next}>
-      <TextTranslation language={language} />
-    </I18nextProvider>
-  )
-}
+const TextTranslationStory: FC<P> = () => <TextTranslation />
+
 export const ENStory = TextTranslationStory.bind({})
 ENStory.args = {
   language: 'en',
-}
-
-export const DEStory = TextTranslationStory.bind({})
-DEStory.args = {
-  language: 'de',
 }
--- libs/ui/src/lib/text-translation/TextTranslation.tsx
+++ libs/ui/src/lib/text-translation/TextTranslation.tsx
@@ -7,13 +7,12 @@ export interface TextTranslationProps {
   language?: string
 }
 
-export const TextTranslation: FC<TextTranslationProps> = ({ language }) => {
-  const { t, i18n } = useTranslation('common')
-  i18n.changeLanguage(language ?? 'en')
+export const TextTranslation: FC<TextTranslationProps> = () => {
+  const { t } = useTranslation('common')
 
   return (
     <div className={styles.contentWrapper}>
-      <h1 className={styles.language}>language:- {language}</h1>
+      <h1 className={styles.language}>language:-</h1>
       <h3 className={styles.title}>Title: - {t('welcome.title')}</h3>
       <p className={styles.desc}>Desciption: - {t('welcome.desc')}</p>
     </div>
