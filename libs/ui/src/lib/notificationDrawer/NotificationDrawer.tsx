4ac64042c244ebb3ca6fb0bf3ec00fb6b01b4f69
sparth786 <sparth786@gmail.com>
2021-02-05 12:53:25 +0530

    notification icons

--- libs/ui/src/lib/notificationDrawer/NotificationDrawer.tsx
+++ libs/ui/src/lib/notificationDrawer/NotificationDrawer.tsx
@@ -5,6 +5,11 @@ import { Drawer, Image } from 'antd'
 import AppointmentSVG from '../../assets/images/notification.svg'
 import ReportSVG from '../../assets/images/notification-report.svg'
 import LeadSVG from '../../assets/images/notification-lead.svg'
+import ReviewSVG from '../../assets/images/review.svg'
+import ReferSVG from '../../assets/images/refer.svg'
+import RequestSVG from '../../assets/images/request.svg'
+import NewsletterSVG from '../../assets/images/newsletter.svg'
+import CampaignSVG from '../../assets/images/campaign.svg'
 import { ReactComponent as EmptySVG } from '../../assets/images/notification-empty.svg'
 import { ReactComponent as Lead1SVG } from '../../assets/images/lead.svg'
 import { ReactComponent as Lead2SVG } from '../../assets/images/lead1.svg'
@@ -77,35 +82,35 @@ export const NotificationDrawer: FC<P> = ({
           {
             notificationTime: '1:21 PM',
             notificationType: 'review',
-            notificationTypeIcon: LeadSVG,
+            notificationTypeIcon: ReviewSVG,
             title: 'New review delivered',
             desc: 'Olivia Sanders has left a new review',
           },
           {
             notificationTime: '1:13 PM',
             notificationType: 'sms campaign',
-            notificationTypeIcon: LeadSVG,
+            notificationTypeIcon: CampaignSVG,
             title: 'New SMS campaign delivered',
             desc: 'Check out new SMS campaign',
           },
           {
             notificationTime: '12:48 PM',
             notificationType: 'Newsletter campaign',
-            notificationTypeIcon: LeadSVG,
+            notificationTypeIcon: NewsletterSVG,
             title: 'New Newsletter campaign delivered',
             desc: 'Check out new newsletter campaign',
           },
           {
             notificationTime: '12:12 PM',
             notificationType: 'holiday request',
-            notificationTypeIcon: LeadSVG,
+            notificationTypeIcon: RequestSVG,
             title: 'Joe Hickey requests a holiday',
             desc: 'Deny or confirm it',
           },
           {
             notificationTime: '10:42 AM',
             notificationType: 'business refer',
-            notificationTypeIcon: LeadSVG,
+            notificationTypeIcon: ReferSVG,
             title: 'Someone refers into the business',
             desc: 'Click to learn more',
           },
