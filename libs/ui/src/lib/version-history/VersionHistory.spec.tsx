fa09180e4918987157742dede47b2df2705af5da
Rocky Wang <wang.mythaero@gmail.com>
2021-02-07 11:12:00 -0800

    Refactor codebase

--- libs/ui/src/lib/version-history/VersionHistory.spec.tsx
+++ libs/ui/src/lib/version-history/VersionHistory.spec.tsx
@@ -5,7 +5,31 @@ import VersionHistory from './VersionHistory'
 
 describe('VersionHistory', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<VersionHistory />)
+    const { baseElement } = render(
+      <VersionHistory
+        history={{
+          last_week: [
+            {
+              version: 'Current verison',
+              updatedBy: 'William Brandham',
+              date: 'January 22, 2:27 PM',
+            },
+            {
+              version: 'Version 2',
+              updatedBy: 'Meri Redjepi',
+              date: 'January 22, 1:26 PM',
+            },
+            {
+              version: 'Version 1',
+              updatedBy: 'Meri Redjepi',
+              date: 'January 22, 2:27 PM',
+            },
+          ],
+        }}
+        visible={true}
+        onVisibleChange={() => true}
+      />
+    )
     expect(baseElement).toBeTruthy()
   })
 })
