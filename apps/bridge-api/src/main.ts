b95d0cbc2416ab03ebe0abee39584dbe4e9c7a0f
LeonSoft <newthemeground@gmail.com>
2021-02-08 17:55:00 +0000

    Merged in api_bridge_autogen_cli (pull request #145)
    Api bridge autogen cli
    
    * CI: Debug high memory usage
    
    * CI: Debug high memory usage
    
    * [skip ci] bumping version (patch)
    
    * CI: Debug high memory usage
    
    * [skip ci] bumping version (patch)
    
    * Merged in PAB2-564 (pull request #137)
    
    PAB2-564
    
    * PAB2-564/completed-multiple-selection-checkbox
    
    * PAB2-564/resolving-issues-for-lint
    
    Approved-by: James Colton
    
    * [skip ci] bumping version (patch)
    
    * CI: 2x for now
    
    * [skip ci] bumping version (patch)
    
    * Merged master into api_bridge_autogen_cli
    
    Approved-by: James Colton

---
+++ apps/bridge-api/src/main.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from 'apollo-server'
+import { schema } from './schema'
+import { createContext } from './context'
+
+new ApolloServer({
+  schema,
+  context: createContext,
+  tracing: true,
+}).listen(
+  { port: 4000 },
+  () =>
+    console.log(
+      `🚀 Server ready at: http://localhost:4000`,
+    ),
+)
