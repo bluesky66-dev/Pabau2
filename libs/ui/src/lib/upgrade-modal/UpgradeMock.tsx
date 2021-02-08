3b4ae636e883b7a021975c1f5df44ae03780b52a
mamtarathoreninja <mamtarathoreninja@gmail.com>
2021-02-08 17:25:43 +0530

    Upgrade modal (marketing)

---
+++ libs/ui/src/lib/upgrade-modal/UpgradeMock.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { TeamOutlined } from '@ant-design/icons'
+import { ReactComponent as Shuffle } from '../../assets/images/shuffle.svg'
+import { ReactComponent as ApplicationSave } from '../../assets/images/application-save.svg'
+import { ReactComponent as ApplicationBook } from '../../assets/images/application-book.svg'
+
+export const title = 'Upgrade to Accelerate plan'
+export const plan = 'from $499.00/month'
+export const description =
+  'We’ll ask you about your needs, help you understand ow pricing works, and get you started with new features.'
+export const btnText = 'Chat with us'
+export const sectionTitle = 'Scale with advanced workflows and security'
+export const sectionData = [
+  {
+    icon: <TeamOutlined />,
+    title: 'Collaborate as a team',
+    description: 'Drive faster responses with shared team inboxes',
+  },
+  {
+    icon: <Shuffle />,
+    title: 'Automate team workflows',
+    description: 'Leverage powerful assignment rules to automate workflows',
+  },
+  {
+    icon: <ApplicationSave />,
+    title: 'Reduce workload with bots',
+    description: 'Automate actions with customizable conversational bots',
+  },
+  {
+    icon: <ApplicationBook />,
+    title: 'Provide global support',
+    description: 'Support customers with a multilingual help center',
+  },
+]
+export const sectionUpgradeTitle = 'Everything you get with the upgrade'
+export const sectionUpgradeData = [
+  'Create private articles',
+  'Reactions',
+  'Audience targeting',
+  'Round robin assignment',
+  'Button triggers',
+  'Salesforce integration',
+  'Follow up actions',
+  'Serve articles to logged-in users',
+  'Bot identity',
+  'Target inbound conversations',
+  'Bot Inbox',
+  'Multiple team inboxes',
+  'Auto-close abandoned bot conversations',
+  'Team office hours',
+  'Preview',
+  'Twitter integration',
+  'Reporting',
+  'Unbranded messaging',
+  'Bot rules',
+  'Remove intercom branding',
+  'Bot templates',
+  'Zendesk integration',
+  'Video bots',
+  'Customizable reporting charts',
+  'Unlimited bots',
+  'Conversation attributes',
+  'Facebook integration',
+  'Conversational Insights',
+  'Priority rules',
+  'Upfront collection bot',
+  'Assignment rules',
+  'Effectiveness report',
+  'Support 38 languages',
+  'Team performance report',
+  'Sales team performance reports',
+  'Duplicate conversations bot',
+]
+export const linkText = 'Compare all plans'
