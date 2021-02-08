3e72ec4758a86c61e353b2f94d1e543fc134ceca
James Colton <james.c@pabau.com>
2021-02-08 22:20:38 +0000

    CI: Vercel teams

--- libs/ui/cicd/deploy.sh
+++ libs/ui/cicd/deploy.sh
@@ -78,7 +78,7 @@ HEREDOC
 
 else
   echo "===== Processing type PR ====="
-  OUTPUT=$(cd "${build_output_path}/" && vercel -c -C --token "${VERCEL_TOKEN}" --scope Pabau2 -A ./vercel.json)
+  OUTPUT=$(cd "${build_output_path}/" && vercel -c -C --token "${VERCEL_TOKEN}" --scope pabau2 -A ./vercel.json)
   echo "errorlevel: $?"
   echo "Output from vercel:"
   echo "${OUTPUT}"
