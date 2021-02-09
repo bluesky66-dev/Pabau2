03b5f7498d14f25085690f3df675484be55662da
= <bilal4168@gmail.com>
2021-02-09 23:55:37 +0500

    PAB2-571/initial-commit-to-push-the-branch

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/documents/Documents'
 export * from './lib/add-suggestion/AddSuggestion'
 export * from './lib/daily/Daily'
 export * from './lib/business-details-notifications/BusinessDetailsNotifications'
---
+++ libs/ui/src/lib/documents/Documents.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Documents.module.less'
+
+/* eslint-disable-next-line */
+export interface DocumentsProps {}
+
+export function Documents(props: DocumentsProps) {
+  return (
+    <div>
+      <h1>Welcome to Documents!</h1>
+    </div>
+  )
+}
+
+export default Documents
