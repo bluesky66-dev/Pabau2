5fd3d6bdb29c386903add5f1f906c0b2e0fc36c9
Rocky Wang <wang.mythaero@gmail.com>
2021-02-10 14:01:43 +0000

    Merged in hf_rocky_clientnotification (pull request #161)
    Hf rocky clientnotification
    
    * Upgrade modal (marketing)
    
    * Reslove the conflicts and update with master
    
    * Update the plus component, trigger modal on plus button
    
    * Quick fixes
    
    * Merged master into PAB2-576
    
    * Merged master into PAB2-576
    
    * Fix the issue occurred while pre-rendering /clientnotification page
    
    * Refactor codebase for UpgradeModal
    
    Approved-by: James Colton

--- libs/ui/src/lib/badge/Badge.stories.tsx
+++ libs/ui/src/lib/badge/Badge.stories.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Badge, PabauPlus } from './Badge'
+import { PlusProps } from './Badge'
 
 export default {
   component: Badge,
   title: 'Basics/Badge',
-  args: { label: 'Enabled', disabled: false },
+  args: {
+    label: 'Enabled',
+    disabled: false,
+    modalType: 'Marketing',
+  },
   argTypes: {
     label: { control: { type: 'text' } },
     disabled: { control: { type: 'boolean' } },
+    modalType: {
+      control: {
+        type: 'select',
+        options: ['Marketing', 'Care', 'Staff', 'Intelligence'],
+      },
+    },
     onClick: { action: 'clicked' },
   },
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -21,8 +32,9 @@ defaultStatusLabel.args = {
   disabled: false,
 }
 
-const PabauPlusStory = ({ ...args }) => <PabauPlus {...args} />
+const PabauPlusStory = ({ ...args }: PlusProps) => <PabauPlus {...args} />
 export const pabauPlus = PabauPlusStory.bind({})
 pabauPlus.args = {
   label: 'Plus',
+  modalType: 'Marketing',
 }
