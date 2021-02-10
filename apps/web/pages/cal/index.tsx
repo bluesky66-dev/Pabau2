05f244de88bd98ab002dd0c9a581e04bcca316ff
James Colton <james.c@pabau.com>
2021-02-10 14:34:11 +0000

    Add iframe for testing

--- apps/web/pages/cal/index.tsx
+++ apps/web/pages/cal/index.tsx
@@ -2,7 +2,16 @@ import React, { FC } from 'react'
 import { Layout } from '@pabau/ui'
 
 const Index: FC = () => {
-  return <Layout pageTitle="Cal">cal to go here..</Layout>
+  return (
+    <Layout pageTitle="Cal">
+      <iframe
+        title="Pabau"
+        src="https://crm.pabau.com/cal"
+        width="100%"
+        height="70vh"
+      />
+    </Layout>
+  )
 }
 
 export default Index
