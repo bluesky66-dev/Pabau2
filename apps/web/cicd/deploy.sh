0af038d0f4b0248d08df82939bf07da7f80959b8
James Colton <james.c@pabau.com>
2021-02-10 20:10:11 +0000

    Merged in ci_improve_pr_pipeline (pull request #166)
    Ci improve pr pipeline
    
    * test2
    
    * test3
    
    * fully-root the path to vercel.json
    
    * fully-root the path to vercel.json
    
    * point to Pabau cal with iframe
    
    * Iframe component
    
    * Fixed Quick Create popup not hiding properly
    
    * Fixed Quick Create popup not hiding properly
    
    * [skip ci] CI: remove ".tsx" from urls
    
    * Merged master into ci_improve_pr_pipeline

--- apps/web/cicd/deploy.sh
+++ apps/web/cicd/deploy.sh
@@ -18,18 +18,21 @@ read_heredoc() {
   eval $1'=${read_heredoc_result}'
 }
 
-echo "----- DEBUG -----"
-echo "(pwd)=$(pwd)"
 APP_NAME="$(basename "$(dirname "$(
   cd "$(dirname "${0}")"
   pwd
 )")")"
-echo "APP_NAME=${APP_NAME}"
 APP_TYPE="$(basename "$(dirname "$(
   cd "$(dirname "${0}")/.."
   pwd
 )")")"
+VERCEL_JSON_LOCATION=$(cd "${APP_TYPE}/${APP_NAME}" && pwd)
+
+echo "----- DEBUG -----"
+echo "(pwd)=$(pwd)"
+echo "APP_NAME=${APP_NAME}"
 echo "APP_TYPE=${APP_TYPE}"
+echo "VERCEL_JSON_LOCATION=${VERCEL_JSON_LOCATION}"
 echo "BITBUCKET_COMMIT=${BITBUCKET_COMMIT}"
 echo "BITBUCKET_PR_ID=${BITBUCKET_PR_ID}"
 pwd
@@ -46,18 +49,17 @@ else
   echo "ERROR: unknown app type '${APP_TYPE}'"; exit 1
 fi
 
-cp "${APP_TYPE}/${APP_NAME}/vercel.json" "${build_output_path}/"
-
 if [ -z "${BITBUCKET_PR_ID}" ]; then
+
   echo "===== Processing type COMMIT ====="
-  OUTPUT=$(cd "${build_output_path}/" && vercel -c -C --token "${VERCEL_TOKEN}" --scope pabau2 -A ./vercel.json --prod)
+  OUTPUT=$(cd "${build_output_path}" && vercel -c -C --token "${VERCEL_TOKEN}" --scope pabau2 -A "${VERCEL_JSON_LOCATION}/vercel.json" --prod)
   echo "errorlevel: $?"
   echo "Output from vercel:"
   echo "${OUTPUT}"
   echo "--"
   LAST_LINE=$(echo "${OUTPUT}" | tail -n1)
   echo "last line: ${LAST_LINE}"
-  echo "${LAST_LINE}" > /tmp/bot_url_web.txt
+  echo "${LAST_LINE}" > /tmp/bot_url_${APP_NAME}.txt
 
   message_body=''
   read_heredoc message_body <<HEREDOC
@@ -65,26 +67,15 @@ ${APP_NAME}: ${LAST_LINE}
 HEREDOC
   echo "${message_body}" >> /tmp/bot_message.txt
 
-  cat tools/cicd/slack_notification.json > /dev/null || (echo "ERROR: JSON not found"; exit 1)
-  jq '.' tools/cicd/slack_notification.json > /dev/null || (echo "ERROR: Invalid JSON"; exit 1)
-
-#  jq --arg var "${message_body}" '.blocks[0].text.text = $var' tools/cicd/slack_notification.json | curl -0 "${SLACK_HOOK_URL}" \
-#    -H "Expect:" \
-#    -H 'Content-Type: application/json; charset=utf-8' \
-#    --data-binary @-
-
 else
   echo "===== Processing type PR ====="
-  OUTPUT=$(cd "${build_output_path}/" && vercel -c -C --token "${VERCEL_TOKEN}" --scope pabau2 -A ./vercel.json)
+  OUTPUT=$(cd "${build_output_path}" && vercel -c -C --token "${VERCEL_TOKEN}" --scope pabau2 -A "${VERCEL_JSON_LOCATION}/vercel.json")
   echo "errorlevel: $?"
   echo "Output from vercel:"
   echo "${OUTPUT}"
   echo "--"
   LAST_LINE=$(echo "${OUTPUT}" | tail -n1)
   echo "last line: ${LAST_LINE}"
-  curl -X POST -H "Content-Type: application/json" \
-    -d '{"channel":"#pabau-2-dev","text":"'"${APP_NAME}"' for PR ID '"${BITBUCKET_PR_ID}"' https://bitbucket.org/pabau/monorepo/pull-requests/'"${BITBUCKET_PR_ID}"' by '"${PR_AUTHOR}"' deployed to '"${LAST_LINE}"'"}' \
-    "${SLACK_HOOK_URL}"
+  echo "${LAST_LINE}" > /tmp/bot_url_${APP_NAME}.txt
 
-  #vercel --token "${VERCEL_TOKEN}" alias "${LAST_LINE}" "crm-pr-${BITBUCKET_PR_ID}.new.pabau.com"
 fi
