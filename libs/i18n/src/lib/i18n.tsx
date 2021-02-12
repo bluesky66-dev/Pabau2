bb66eda330ac1c2eb109758aca4599952a53a1bb
James Colton <james.c@pabau.com>
2021-02-12 10:38:54 +0000

    add new lib - i18n

---
+++ libs/i18n/src/lib/i18n.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './i18n.module.less'
+
+/* eslint-disable-next-line */
+export interface I18nProps {}
+
+export function I18n(props: I18nProps) {
+  return (
+    <div>
+      <h1>Welcome to i18n!</h1>
+    </div>
+  )
+}
+
+export default I18n
