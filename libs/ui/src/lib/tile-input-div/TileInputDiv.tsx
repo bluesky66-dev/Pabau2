6cfaeaacdd37bd8904e4390fcf6a8c54698fefae
Suhani Shah <suhanishah228@gmail.com>
2021-02-15 17:20:44 +0530

    completed TileInputDiv with designing

--- libs/ui/src/lib/tile-input-div/TileInputDiv.tsx
+++ libs/ui/src/lib/tile-input-div/TileInputDiv.tsx
@@ -1,12 +1,18 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { PhoneNumberInput, Input } from '@pabau/ui'
 import { Form } from 'antd'
-import './TileInputDiv.module.less'
+import styles from './TileInputDiv.module.less'
 import { useFormik } from 'formik'
 
 interface inputProps {
   label?: string
   placeholderText?: string
+}
+
+interface inputTypes {
+  companyName?: string,
+  phone?: string,
+  website?: string
 }
 
 /* eslint-disable-next-line */
@@ -23,14 +29,31 @@ export const TileInputDiv: FC<TileInputDivProps> = ({
   phoneLabel,
   websiteData,
 }) => {
+  const [ focused, setFocused ] = useState(false);
+  const validate = (values) => {
+    const errors: inputTypes = {};
+
+    if(!values.companyName) {
+      errors.companyName = 'Company name is required';
+    }
+
+    if(!values.website) {
+      errors.website = 'Website is required';
+    } else if (!/^[A-Z0-9._%+-]+\.[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.website)) {
+      errors.website = 'Invalid website name';
+    }
+    
+    return errors; 
+  }
   const formik = useFormik({
     initialValues: {
       companyName: '',
       phone: '',
       website: '',
     },
+    validate,
     onSubmit: (values) => {
-      // alert(JSON.stringify(values, null, 2));
+      console.log("Form Vales:", values);
     },
   })
 
@@ -38,29 +61,42 @@ export const TileInputDiv: FC<TileInputDivProps> = ({
     formik.setFieldValue(key, name)
   }
 
+  const handleFocusElement = (status) => {
+    setFocused(status);
+  }
+  
   return (
-    <div>
-      <div>
+    <div className={styles.mainWrapper}>
+      <div className={ focused ? styles.focusedContentWrapper : styles.contentWrapper}
+        onFocus={() => handleFocusElement(true)}
+        onBlur={() => handleFocusElement(false)}
+      >
         <h3>{title}</h3>
-        <Form>
-          <Input
-            label={companyNameData?.label}
-            placeHolderText={companyNameData?.placeholderText}
-            onChange={(e) => onchange(e, 'companyName')}
-            text={formik.values.companyName}
-          />
-          <div style={{ marginTop: '30px', marginBottom: '10px' }}>
+        <Form onSubmitCapture={formik.handleSubmit}>
+          <div className={styles.customForm}>
+            <Input
+              label={companyNameData?.label}
+              placeHolderText={companyNameData?.placeholderText}
+              onChange={(e) => onchange(e, 'companyName')}
+              text={formik.values.companyName}
+            />
+            {formik.errors.companyName ? <div className={styles.error}>{formik.errors.companyName}</div> : null}
+          </div>
+          <div className={styles.customForm}>
             <PhoneNumberInput
               label={phoneLabel}
               onChange={(e) => onchange(e, 'phone')}
             />
           </div>
-          <Input
-            label={websiteData?.label}
-            placeHolderText={websiteData?.placeholderText}
-            onChange={(e) => onchange(e, 'website')}
-            text={formik.values.website}
-          />
+          <div className={styles.customForm}>
+            <Input
+              label={websiteData?.label}
+              placeHolderText={websiteData?.placeholderText}
+              onChange={(e) => onchange(e, 'website')}
+              text={formik.values.website}
+            />
+            {formik.errors.website ? <div className={styles.error}>{formik.errors.website}</div> : null}
+          </div>
         </Form>
       </div>
     </div>
