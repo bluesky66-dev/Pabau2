25342a0e572e6da16abc12180e2736f2476ca5c2
tboshkovski <toshe@pabau.com>
2021-02-15 14:44:12 +0100

    shell script for deployment

--- apps/bridge-api/cicd/deploy.sh
+++ apps/bridge-api/cicd/deploy.sh
@@ -3,8 +3,14 @@ set -e
 
 echo "DEBUG: pwd=$(pwd)"
 APP_NAME="$(basename "$(dirname "$(cd "$(dirname "${0}")"; pwd)")")"
+DOCKER_USERNAME="ops"
+DOCKER_PASSWORD="jfdjiwfdjwiojdsmcnvhsbakdujr"
 echo "DEBUG: app_name=${APP_NAME}"
 
 yarn run nx run "${APP_NAME}:build" --prod
 cp "apps/${APP_NAME}/package.json-prod" "dist/apps/${APP_NAME}/"
 cp -r "apps/${APP_NAME}/prisma" "dist/apps/${APP_NAME}/prisma"
+docker build . -t legacy-bridge -f tools/cicd/bridge.Dockerfile
+docker image tag "${APP_NAME}:latest" "registry.pabau.com:5000/monorepo/${APP_NAME}"
+docker login registry.pabau.com:5000 -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+docker image push "registry.pabau.com:5000/monorepo/${APP_NAME}"
