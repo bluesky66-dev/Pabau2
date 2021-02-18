8db22fc4ed0db9c6c22dca202e82b947143e729d
bigsmall2697 <bigsmall2697@gmail.com>
2021-02-18 10:16:33 -0500

    Update testing script

--- libs/ui/src/lib/reports-permission-table/ReportsPermissionTable.spec.tsx
+++ libs/ui/src/lib/reports-permission-table/ReportsPermissionTable.spec.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 import { render } from '@testing-library/react'
 
 import ReportsPermissionTable from './ReportsPermissionTable'
+import { title, subtitle, data, columns } from './mock'
 
 describe('ReportsPermissionTable', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<ReportsPermissionTable />)
+    const { baseElement } = render(
+      <ReportsPermissionTable
+        dataSource={data}
+        columns={columns}
+        pageTitle={title}
+        subTitle={subtitle}
+      />
+    )
     expect(baseElement).toBeTruthy()
   })
 })
