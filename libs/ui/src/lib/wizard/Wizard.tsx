09ebe33d72e510de11047aceffc7d7265581d0d8
Dipak Savaliya <dipak241@gmail.com>
2021-02-23 19:59:00 +0530

    Resolved wizard bug

--- libs/ui/src/lib/wizard/Wizard.tsx
+++ libs/ui/src/lib/wizard/Wizard.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styels from './Wizard.module.less'
 import { Button } from '@pabau/ui'
 import { StepperInterface } from './mock'
+import Stepper from '../stepper/Stepper'
+
 interface WizardProps {
   onPrev?: () => void
   onNext?: () => void
@@ -15,28 +17,41 @@ export const Wizard: React.FC<WizardProps> = ({
   onPrev,
   onNext,
   active,
+  stepperData,
   allSteps,
   disableNextStep = false,
+  children,
 }) => {
   return (
-    <div className={styels.footer}>
-      <Button
-        onClick={(event) => onPrev?.()}
-        disabled={active < 1 || active === allSteps - 1}
-      >
-        Previous Step
-      </Button>
-      <span className={styels.breadcrumbgraytxt}>
-        Step {active + 1}/{allSteps}
-      </span>
+    <div className={styels.container}>
+      <hr className={styels.line} />
 
-      <Button
-        type="primary"
-        onClick={(event) => onNext?.()}
-        disabled={disableNextStep || allSteps - 1 <= active}
-      >
-        Next Step
-      </Button>
+      <Stepper datasource={stepperData} step={active} />
+
+      <hr className={styels.bottomline} />
+
+      {children}
+
+      {/* <div style={{ marginTop: '20px' }}></div> */}
+      <div className={styels.footer}>
+        <Button
+          onClick={(event) => onPrev?.()}
+          disabled={active < 1 || active === allSteps - 1}
+        >
+          Previous Step
+        </Button>
+        <span className={styels.breadcrumbgraytxt}>
+          Step {active + 1}/{allSteps}
+        </span>
+
+        <Button
+          type="primary"
+          onClick={(event) => onNext?.()}
+          disabled={disableNextStep || allSteps - 1 <= active}
+        >
+          Next Step
+        </Button>
+      </div>
     </div>
   )
 }
