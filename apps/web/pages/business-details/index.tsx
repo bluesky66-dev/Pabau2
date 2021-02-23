fa99c9fa40165209a3df2bae0b2ec004e30bce38
Himanshu Kumar <himanshukumar077@gmail.com>
2021-02-23 14:02:16 +0530

    PAB2-500

--- apps/web/pages/business-details/index.tsx
+++ apps/web/pages/business-details/index.tsx
@@ -12,7 +12,7 @@ import {
 import Layout from '../../components/Layout/Layout'
 import CommonHeader from '../setup/CommonHeader'
 import styles from './index.module.less'
-
+import { gql, useMutation } from '@apollo/client'
 const { Title } = Typography
 
 export const Index: FC = () => {
@@ -23,6 +23,54 @@ export const Index: FC = () => {
     'Security',
     'Notifications',
   ]
+  const ADD_MUTATION = gql`
+    mutation insert_company_details_one($data: CompanyDetailsCreateInput!) {
+      createOneCompanyDetails(data: $data) {
+        company_id
+      }
+    }
+  `
+  const [createBusinessDetails] = useMutation(ADD_MUTATION, {
+    fetchPolicy: 'no-cache',
+  })
+  const onSave = async (data) => {
+    const { basicInformation, businessLocation, languageSetting } = data
+    console.log(data)
+    const {
+      businessName,
+      businessType,
+      companyEmail,
+      phone,
+      website,
+    } = basicInformation
+    const {
+      currency,
+      dateFormat,
+      defaultLanuageClients,
+      defaultLanuageStaff,
+      timezone,
+      weekStart,
+    } = languageSetting
+    try {
+      await createBusinessDetails({
+        variables: {
+          data: {
+            company_name: businessName || 'Business Name',
+            currency,
+            dateFormat,
+            week_start_day: weekStart,
+            website,
+            phone,
+            info_email: companyEmail,
+            industry_sector: 'industry_sector',
+            timezone,
+          },
+        },
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <>
       <CommonHeader />
@@ -38,20 +86,20 @@ export const Index: FC = () => {
         </div>
         <div className={styles.tabsForDesktop}>
           <TabMenu tabPosition="left" menuItems={tabMenuItems} minHeight="auto">
-            <BusinessDetails />
-            <Terminology />
-            <System />
+            <BusinessDetails onSave={onSave} />
+            <Terminology onSave={onSave} />
+            <System onSave={onSave} />
             <Security />
-            <BusinessDetailsNotifications />
+            <BusinessDetailsNotifications onSave={onSave} />
           </TabMenu>
         </div>
         <div className={styles.tabsForMobile}>
           <TabMenu tabPosition="top" menuItems={tabMenuItems} minHeight="auto">
-            <BusinessDetails />
-            <Terminology />
-            <System />
+            <BusinessDetails onSave={onSave} />
+            <Terminology onSave={onSave} />
+            <System onSave={onSave} />
             <Security />
-            <BusinessDetailsNotifications />
+            <BusinessDetailsNotifications onSave={onSave} />
           </TabMenu>
         </div>
       </Layout>
