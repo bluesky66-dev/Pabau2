0a6a7a40e6c19940e4cc45618d3f274b1f81eb7b
William Xin <bigsmall2697@gmail.com>
2021-02-23 10:35:38 -0500

    Fix testing

--- libs/ui/src/lib/reports-permissions/ReportsPermissions.spec.tsx
+++ libs/ui/src/lib/reports-permissions/ReportsPermissions.spec.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 import { render } from '@testing-library/react'
 
 import ReportsPermissions from './ReportsPermissions'
+import { title, subtitle, data, permissions } from './data'
 
 describe('ReportsPermissions', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<ReportsPermissions />)
+    const { baseElement } = render(
+      <ReportsPermissions
+        data={data}
+        permissions={permissions}
+        pageTitle={title}
+        subTitle={subtitle}
+      />
+    )
     expect(baseElement).toBeTruthy()
   })
 })
