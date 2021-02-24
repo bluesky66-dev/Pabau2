8f25f192714c6ca6b70079fddccc4d7e538b250b
tboshkovski <toshe@pabau.com>
2021-02-24 18:10:38 +0100

    wip auth

--- apps/bridge-api/src/main.ts
+++ apps/bridge-api/src/main.ts
@@ -1,21 +1,39 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express';
 import { schema } from './schema'
 import { createContext } from './context'
+import bodyParser from 'body-parser'
+import { LoginHandler} from "./app/authentication/login/LoginHandler";
+
 
 const PORT = 4000;
-
+const app = express();
 const server = new ApolloServer({
   schema,
   context: createContext,
   tracing: true,
 });
+app.use(bodyParser.json());
 
-server.listen(
+app.post('/login', async (req, res) => {
+  try{
+    const login = await new LoginHandler(req, res).handleRequest();
+    return res.json({
+      accessToken: login
+    });
+  } catch (error){
+    console.log(error)
+  }
+});
+
+server.applyMiddleware({app})
+
+app.listen(
   { port: PORT },
   () =>
     console.log(
       `Server running on port ${PORT}`,
     ),
-).catch(error => console.error(error))
+)
 
 
