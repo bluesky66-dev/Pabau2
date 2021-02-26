790ccaf61ba189021dfdf1e0742ef3a0f7189855
Suhani Shah <suhanishah228@gmail.com>
2021-02-26 18:28:02 +0530

    PAB2-643

--- apps/web/pages/setup/third-parties.tsx
+++ apps/web/pages/setup/third-parties.tsx
@@ -1,163 +1,367 @@
-// import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+  Breadcrumb,
+  Button,
+  Notification,
+  NotificationType,
+  Table,
+  useLiveQuery,
+  Pagination,
+  TabbedTable
+} from '@pabau/ui'
+import { Card, Col, Row, Typography } from 'antd'
+//import NewThirdTypeModal from '../../components/Setup/Third-Party/Third-partiesModal'
+import Layout from '../../components/Layout/Layout'
+import styles from './third-parties.module.less'
+import { gql, useMutation } from '@apollo/client'
 
-// import './third-parties.module.less'
+/* eslint-disable-next-line */
+export interface BlockOutOptionsProps {}
 
-// /* eslint-disable-next-line */
-// export interface ThirdPartiesProps {}
-
-// export function ThirdParties(props: ThirdPartiesProps) {
-//   return (
-//     <div>
-//       <h1>Welcome to third-parties!</h1>
-//     </div>
-//   )
-// }
-
-// export default ThirdParties
-import { gql } from '@apollo/client'
-import { NextPage } from 'next'
-import React from 'react'
-import CrudLayout from '../../components/CrudLayout/CrudLayout'
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    className: 'drag-visible',
+    visible: true,
+  },
+  {
+    title: 'Type',
+    dataIndex: 'type',
+    className: 'drag-visible',
+    visible: true,
+  },
+  {
+    title: 'Status',
+    dataIndex: 'is_active',
+    className: 'drag-visible',
+    visible: true,
+    width: '160px',
+  },
+]
 
 const LIST_QUERY = gql`
-  query departments(
-    $isActive: Boolean = true
-    $offset: Int
-    $limit: Int
-    $searchTerm: String = ""
-  ) {
-    departments(
+  query block_out_options($offset: Int, $limit: Int) {
+    block_out_options(
       offset: $offset
       limit: $limit
-      order_by: { order: desc }
-      where: {
-        is_active: { _eq: $isActive }
-        _or: [{ _and: [{ name: { _ilike: $searchTerm } }] }]
-      }
+      order_by: { created_at: desc }
     ) {
-      __typename
       id
       name
+      paidBlockOut
       is_active
-      order
+      type
+      backgroundColor
+      defaultTime
     }
   }
 `
-const LIST_AGGREGATE_QUERY = gql`
-  query departments_aggregate(
-    $isActive: Boolean = true
-    $searchTerm: String = ""
+
+const ADD_MUTATION = gql`
+  mutation insert_block_out_options_one(
+    $name: String
+    $type: String
+    $paidBlockOut: Boolean = false
+    $backgroundColor: String
+    $defaultTime: Int
+    $isActive: Boolean
   ) {
-    departments_aggregate(
-      where: {
-        is_active: { _eq: $isActive }
-        _or: [{ _and: [{ name: { _ilike: $searchTerm } }] }]
+    insert_block_out_options_one(
+      object: {
+        name: $name
+        type: $type
+        paidBlockOut: $paidBlockOut
+        backgroundColor: $backgroundColor
+        defaultTime: $defaultTime
+        is_active: $isActive
       }
     ) {
+      id
+      name
+      paidBlockOut
+      backgroundColor
+      defaultTime
+    }
+  }
+`
+
+const EDIT_MUTATION = gql`
+  mutation update_job_title_by_pk(
+    $id: uuid!
+    $name: String
+    $type: String
+    $paidBlockOut: Boolean = false
+    $backgroundColor: String
+    $defaultTime: Int
+    $isActive: Boolean
+  ) {
+    update_block_out_options_by_pk(
+      pk_columns: { id: $id }
+      _set: {
+        name: $name
+        type: $type
+        paidBlockOut: $paidBlockOut
+        backgroundColor: $backgroundColor
+        defaultTime: $defaultTime
+        is_active: $isActive
+      }
+    ) {
+      id
+    }
+  }
+`
+
+const DELETE_MUTATION = gql`
+  mutation delete_block_out_options_by_pk($id: uuid!) {
+    delete_block_out_options_by_pk(id: $id) {
+      id
+    }
+  }
+`
+
+const LIST_AGGREGATE_QUERY = gql`
+  query block_out_options_aggregate {
+    block_out_options_aggregate {
       aggregate {
         count
       }
     }
   }
 `
-const DELETE_MUTATION = gql`
-  mutation delete_departments_by_pk($id: uuid!) {
-    delete_departments_by_pk(id: $id) {
-      __typename
-      id
-    }
-  }
-`
-const ADD_MUTATION = gql`
-  mutation insert_departments_one($name: String!, $is_active: Boolean) {
-    insert_departments_one(object: { name: $name, is_active: $is_active }) {
-      __typename
-      id
-    }
-  }
-`
-const EDIT_MUTATION = gql`
-  mutation update_departments_by_pk(
-    $id: uuid!
-    $name: String!
-    $is_active: Boolean
-    $order: Int
-  ) {
-    update_departments_by_pk(
-      pk_columns: { id: $id }
-      _set: { name: $name, is_active: $is_active, order: $order }
-    ) {
-      __typename
-      id
-      is_active
-      order
-    }
-  }
-`
 
-const UPDATE_ORDER_MUTATION = gql`
-  mutation update_departments_order($id: uuid!, $order: Int) {
-    update_departments(where: { id: { _eq: $id } }, _set: { order: $order }) {
-      affected_rows
+export function BlockOutOptions(props: BlockOutOptionsProps) {
+  const { Paragraph, Title } = Typography
+
+  const [showModal, setShowModal] = useState(false)
+  const [edit, setEdit] = useState(null)
+  const [paginateData, setPaginateData] = useState({
+    currentPage: 0,
+    total: 0,
+    offset: 0,
+    limit: 10,
+    showingRecords: 0,
+  })
+
+  const { data, error, loading } = useLiveQuery(LIST_QUERY, {
+    variables: {
+      offset: paginateData.offset,
+      limit: paginateData.limit,
+    },
+  })
+
+  const { data: aggregateData } = useLiveQuery(LIST_AGGREGATE_QUERY)
+
+  const [addMutation] = useMutation(ADD_MUTATION, {
+    onCompleted() {
+      Notification(
+        NotificationType.success,
+        `Success! You have successfully created a blockout option`
+      )
+    },
+    onError() {
+      Notification(
+        NotificationType.error,
+        `Error! While creating a blockout option`
+      )
+    },
+  })
+
+  const [editMutation] = useMutation(EDIT_MUTATION, {
+    onCompleted() {
+      Notification(
+        NotificationType.success,
+        `Success! You have successfully edited a blockout option`
+      )
+    },
+    onError() {
+      Notification(
+        NotificationType.error,
+        `Error! While editing a blockout option`
+      )
+    },
+  })
+
+  const [deleteMutation] = useMutation(DELETE_MUTATION, {
+    onCompleted() {
+      Notification(
+        NotificationType.success,
+        `Success! You have successfully deleted a blockout option`
+      )
+    },
+    onError() {
+      Notification(
+        NotificationType.error,
+        `Error! While deleting a blockout option`
+      )
+    },
+  })
+
+  useEffect(() => {
+    if (aggregateData) {
+      setPaginateData({
+        ...paginateData,
+        total: aggregateData.aggregate?.count,
+        showingRecords: data?.length,
+      })
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, aggregateData])
+
+  const onRowClick = (data) => {
+    setEdit(data)
+    setShowModal(true)
   }
-`
 
-const schema: Schema = {
-  full: 'Third Parties',
-  fullLower: 'third parties',
-  short: 'Third Parties',
-  shortLower: 'third parties',
-  createButtonLabel: 'Create third party',
-  messages: {
-    create: {
-      success: 'You have successfully created a department',
-      error: 'While creating a department',
-    },
-    update: {
-      success: 'You have successfully updated a department',
-      error: 'While updating a department',
-    },
-    delete: {
-      success: 'You have successfully deleted a department',
-      error: 'While deleting a department',
-    },
-  },
-  fields: {
-    name: {
-      full: 'Department Name',
-      fullLower: 'department name',
-      short: 'Name',
-      shortLower: 'name',
-      min: 2,
-      example: 'Surgical Department',
-      // description: 'A friendly name',
-      // extra: <i>Please note: blah blah blahh</i>,
-      cssWidth: 'max',
-      type: 'string',
-    },
-    is_active: {
-      full: 'Status',
-      type: 'boolean',
-      defaultvalue: true,
-    },
-  },
-}
+  const onPaginationChange = (currentPage) => {
+    const offset = paginateData.limit * (currentPage - 1)
+    setPaginateData((d) => ({ ...d, offset, currentPage }))
+  }
 
-export const ThirdParties: NextPage = () => {
+  const createClick = () => {
+    setShowModal(true)
+    setEdit(null)
+  }
+
+  const onSave = async (values) => {
+    setShowModal(false)
+    setEdit(null)
+    await (edit
+      ? editMutation({
+          variables: { ...edit, ...values },
+          optimisticResponse: {},
+          update: (proxy) => {
+            const existing = proxy.readQuery({
+              query: LIST_QUERY,
+            })
+            if (existing) {
+              const key = Object.keys(existing)[0]
+              proxy.writeQuery({
+                query: LIST_QUERY,
+                data: {
+                  [key]: [...existing[key], values],
+                },
+              })
+            }
+          },
+        })
+      : addMutation({
+          variables: values,
+          optimisticResponse: {},
+          update: (proxy) => {
+            const existing = proxy.readQuery({
+              query: LIST_QUERY,
+            })
+            if (existing) {
+              const key = Object.keys(existing)[0]
+              proxy.writeQuery({
+                query: LIST_QUERY,
+                data: {
+                  [key]: [...existing[key], values],
+                },
+              })
+            }
+          },
+        }))
+  }
+
+  const onDelete = async () => {
+    await deleteMutation({
+      variables: { id: edit?.id },
+      optimisticResponse: {},
+      update: (cache) => {
+        const existing = cache.readQuery({
+          query: LIST_QUERY,
+        })
+        if (existing) {
+          // eslint-disable-next-line @typescript-eslint/ban-types
+          const key = Object.keys(existing as object)[0]
+          cache.writeQuery({
+            query: LIST_QUERY,
+            data: {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              [key]: (existing[key] as Record<string, never>).filter(
+                (e) => e.id !== edit?.id
+              ),
+            },
+          })
+        }
+      },
+    })
+    setShowModal(false)
+    setEdit(null)
+  }
+
   return (
-    <CrudLayout
-      schema={schema}
-      tableSearch={true}
-      addQuery={ADD_MUTATION}
-      deleteQuery={DELETE_MUTATION}
-      listQuery={LIST_QUERY}
-      editQuery={EDIT_MUTATION}
-      aggregateQuery={LIST_AGGREGATE_QUERY}
-      updateOrderQuery={UPDATE_ORDER_MUTATION}
-      addFilter={true}
-    />
+    <Layout>
+      <Card bodyStyle={{ padding: 0 }}>
+        <Row className={styles.headerContainer}>
+          <Col>
+            <Breadcrumb
+              breadcrumbItems={[
+                { breadcrumbName: 'Setup', path: 'setup' },
+                { breadcrumbName: 'Block Out Options', path: '' },
+              ]}
+            />
+            <Title>Block Out Options</Title>
+          </Col>
+          <Col>
+            <Button type="primary" onClick={createClick}>
+              New block type
+            </Button>
+          </Col>
+        </Row>
+        <div>
+        <TabbedTable tabItems={['Third parties','Library']}>
+        <Table
+          columns={columns}
+          dataSource={data?.map((d) => ({ ...d, key: d.id }))}
+          onRowClick={onRowClick}
+          loading={loading}
+          noDataText="block type"
+          noDataBtnText="block type"
+          onAddTemplate={createClick}
+          rowKey="id"
+        />
+          <Table
+          columns={columns}
+          dataSource={data?.map((d) => ({ ...d, key: d.id }))}
+          onRowClick={onRowClick}
+          loading={loading}
+          noDataText="block type"
+          noDataBtnText="block type"
+          onAddTemplate={createClick}
+          rowKey="id"
+        />
+
+        </TabbedTable>
+
+        </div>
+     
+      
+        {error && <Paragraph type="danger">{error.message}</Paragraph>}
+      </Card>
+      <Pagination
+        showingRecords={paginateData.showingRecords}
+        defaultCurrent={1}
+        total={paginateData.total}
+        pageSize={paginateData.limit}
+        current={paginateData.currentPage}
+        onChange={onPaginationChange}
+      />
+      {/* {showModal && (
+        <NewThirdTypeModal
+          visible={showModal}
+          onCancel={() => setShowModal(false)}
+          isEdit={!!edit}
+          editData={edit}
+          onSave={onSave}
+          onDelete={onDelete}
+        />
+      )} */}
+    </Layout>
   )
 }
 
-export default ThirdParties
+export default BlockOutOptions
