dcffdd0a4648c8dc5a42a5de9967604237182513
KashifToor <toorkashif369@gmail.com>
2021-03-04 01:40:52 +0500

    PAB2638/completed-design-for-circulations-in-voucher-listing

--- apps/web/pages/setup/giftVouchers/index.tsx
+++ apps/web/pages/setup/giftVouchers/index.tsx
@@ -1,12 +1,22 @@
 import React, { FC, useState, useEffect } from 'react'
 import Layout from './../../../components/Layout/Layout'
-import { TabbedTable, Button, Breadcrumb, VoucherCard } from '@pabau/ui'
+import {
+  SearchOutlined,
+  FilterOutlined,
+  ApartmentOutlined,
+} from '@ant-design/icons'
+import { Input } from 'antd'
+import {
+  TabbedTable,
+  Button,
+  Breadcrumb,
+  VoucherCard,
+  Pagination,
+  Table,
+} from '@pabau/ui'
 import VoucherIcon from '../../../components/Setup/GiftVouchersList/assets/VocherIcon'
 import { Card, Row, Col } from 'antd'
 import styles from './index.module.less'
-
-/* eslint-disable-next-line */
-export interface GiftVouchersProps {}
 
 const giftCardSettings = {
   cardWidth: 500,
@@ -34,9 +44,145 @@ const giftCardSettings = {
   `,
 }
 
-const GiftVouchers: FC<GiftVouchersProps> = () => {
-  const [gifts, setGifts] = useState([])
+const data = [
+  {
+    id: 1,
+    key: '1',
+    name: 'Paracetamol (Acetominophen)',
+    unit: 'Mg',
+    frequency: '1 per day',
+    route: 'Orally',
+    comment: 'Twice per 6 hours after treatment',
+    is_active: 1,
+    index: 0,
+  },
+  {
+    id: 2,
+    key: '2',
+    name: 'From a friend',
+    unit: 'Mg',
+    frequency: 'coffee',
+    route: 'Monday, 4 Feb 2019',
+    comment: 'Twice per 6 hours after treatment',
+    is_active: 0,
+    index: 1,
+  },
+  {
+    id: 3,
+    key: '3',
+    name: 'Instagram',
+    unit: 'Mg',
+    frequency: 'clock',
+    route: 'Orally',
+    comment: 'Twice per 6 hours after treatment',
+    is_active: 0,
+    index: 2,
+  },
+  {
+    id: 4,
+    key: '4',
+    name: 'Imported',
+    unit: 'Mg',
+    frequency: '1 per day',
+    route: 'Orally',
+    comment: 'Twice per 6 hours after treatment',
+    is_active: 1,
+    index: 3,
+  },
+  {
+    id: 5,
+    key: '5',
+    name: 'Walk-in',
+    unit: 'Mg',
+    frequency: '1 per day',
+    route: 'Orally',
+    comment: 'Twice per 6 hours after treatment',
+    is_active: 1,
+    index: 4,
+  },
+  {
+    id: 6,
+    key: '6',
+    name: 'Facebook',
+    unit: 'Mg',
+    frequency: '1 per day',
+    route: 'Orally',
+    comment: 'Twice per 6 hours after treatment',
+    is_active: 0,
+    index: 5,
+  },
+  {
+    id: 7,
+    key: '7',
+    name: 'Fresha',
+    unit: 'Mg',
+    frequency: '1 per day',
+    route: 'Orally',
+    comment: 'Twice per 6 hours after treatment',
+    is_active: 0,
+    index: 6,
+  },
+]
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    className: 'leftPadding',
+    visible: true,
+  },
+  {
+    title: 'Units',
+    dataIndex: 'unit',
+    visible: true,
+  },
+  {
+    title: 'Frequency',
+    dataIndex: 'frequency',
+    visible: true,
+  },
+  {
+    title: 'Route',
+    dataIndex: 'route',
+    visible: true,
+  },
+  {
+    title: 'Comment',
+    dataIndex: 'comment',
+    visible: true,
+  },
+  {
+    title: 'Status',
+    dataIndex: 'is_active',
+    visible: true,
+  },
+  {
+    title: '',
+    dataIndex: 'view',
+    visible: true,
+    width: '150',
+    className: 'lastColumn',
+    render: function renderTableSource() {
+      return (
+        <div>
+          <Button size="large">View</Button>
+        </div>
+      )
+    },
+  },
+]
+
+export interface GiftVouchersProps {
+  title?: string
+}
+
+const GiftVouchers: FC<GiftVouchersProps> = ({ title }) => {
   const tabItems = ['Types', 'Circlulation']
+  const [gifts, setGifts] = useState([])
+  const [searchTerm, setSearchTerm] = useState(null)
+  const [paginationState, setPaginationState] = useState(true)
+  const [dataSource, setDataSource] = useState(null)
+  const [activeTab, setActiveTab] = useState(tabItems[0])
 
   const cardHeader = (
     <div className={styles.header}>
@@ -52,9 +198,27 @@ const GiftVouchers: FC<GiftVouchersProps> = () => {
         <h3 className={styles.drugsHeading}>Gift Vouchers</h3>
       </div>
       <div className="rightDiv">
-        <Button type="primary" size="large">
-          Add Voucher Type
-        </Button>
+        {activeTab === tabItems[0] && (
+          <Button type="primary" size="large">
+            Add Voucher Type
+          </Button>
+        )}
+        {activeTab === tabItems[1] && (
+          <div>
+            <Input
+              className={styles.searchDrugsListing}
+              autoFocus
+              placeholder="Search"
+              suffix={<SearchOutlined style={{ color: '#8C8C8C' }} />}
+              onChange={(e) => {
+                setSearchTerm(e.target.value)
+              }}
+            />
+            <Button type="default" size="large">
+              <FilterOutlined /> Filter
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   )
@@ -83,10 +247,22 @@ const GiftVouchers: FC<GiftVouchersProps> = () => {
         voucherType: 'flowers',
       },
     ])
+    setDataSource(data)
   }, [])
 
   const onTabChange = (tab) => {
-    console.log('TAB:', tab)
+    switch (tabItems[tab]) {
+      case tabItems[0]:
+        setActiveTab(tabItems[0])
+        setPaginationState(false)
+        break
+      case tabItems[1]:
+        setActiveTab(tabItems[1])
+        setPaginationState(true)
+        break
+      default:
+        return
+    }
   }
 
   return (
@@ -94,10 +270,13 @@ const GiftVouchers: FC<GiftVouchersProps> = () => {
       <div className={styles.giftVoucherMain}>
         <Card title={cardHeader}>
           <div className={styles.body}>
-            <TabbedTable tabItems={tabItems} onTabChange={() => onTabChange}>
+            <TabbedTable
+              tabItems={tabItems}
+              onTabChange={(tab) => onTabChange(tab)}
+            >
               <div className={styles.types}>
                 <Row>
-                  {gifts.length < 0 ? (
+                  {gifts.length > 0 ? (
                     gifts.map((gift, key) => (
                       <Col
                         lg={8}
@@ -132,10 +311,31 @@ const GiftVouchers: FC<GiftVouchersProps> = () => {
                   )}
                 </Row>
               </div>
-              <div className={styles.tableSheet}></div>
+              <div className={styles.tableSheet}>
+                <Table
+                  draggable={true}
+                  columns={columns}
+                  searchTerm={searchTerm}
+                  noDataText="Circulation"
+                  noDataIcon={<ApartmentOutlined />}
+                  noDataBtnText="Circulation"
+                  scroll={{ x: 'max-content' }}
+                  dataSource={dataSource}
+                />
+              </div>
             </TabbedTable>
           </div>
         </Card>
+        {paginationState && (
+          <div className={styles.paginationDiv}>
+            <Pagination
+              showingRecords={dataSource?.length}
+              defaultCurrent={1}
+              total={dataSource?.length}
+              pageSize={10}
+            />
+          </div>
+        )}
       </div>
     </Layout>
   )
