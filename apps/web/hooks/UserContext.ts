12edc2fe3e35bef1b96147f13bea6d74aec8d5b5
LeonSoft <newthemeground@gmail.com>
2021-03-03 16:49:20 +0100

    release test

--- apps/web/hooks/UserContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react'
-import { LoginProps } from './useLogin'
-
-export const UserContext = createContext<LoginProps | null>(null)
-
-export const UserProvider = UserContext.Provider
-export const UserConsumer = UserContext.Consumer
