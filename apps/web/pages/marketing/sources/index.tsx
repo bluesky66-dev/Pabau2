2d797f415f7d12aa919dbadfce00ad9fc17dda6d
Jemin Kothari <kothari.jemin.dev@gmail.com>
2021-03-04 16:26:16 +0000

    Merged in PAB2-272 (pull request #291)
    PAB2-272
    
    * Created new component called ContextWrapper and bind on _app.tsx
    
    * Added translation on marketing source page
    
    * Merge branch 'authentication' of bitbucket.org:pabau/monorepo into PAB2-272
    
    * Added missing keys in translation json
    
    Approved-by: William Brandham

--- apps/web/pages/marketing/sources/index.tsx
+++ apps/web/pages/marketing/sources/index.tsx
@@ -3,8 +3,9 @@ import { NextPage } from 'next'
 import React, { useContext, useEffect } from 'react'
 import CrudLayout from '../../../components/CrudLayout/CrudLayout'
 /* eslint-disable graphql/template-strings */
-import { useTranslation } from 'react-i18next'
+import { useTranslationI18 } from '../../../hooks/useTranslationI18'
 import { UserContext } from '../../../context/UserContext'
+import { languageMapper } from '../../../helper/languageMapper'
 
 const LIST_QUERY = gql`
   query marketing_sources(
@@ -95,14 +96,19 @@ const UPDATE_ORDER_MUTATION = gql`
 `
 
 export const Index: NextPage = () => {
-  const { t } = useTranslation('common')
+  const { t, i18n } = useTranslationI18()
 
   const user = useContext(UserContext)
 
+  console.log('asdasdasdasdasd', user)
+
   useEffect(() => {
-    console.log('This is context')
-    console.log('+++++++', user)
-  })
+    if (user) {
+      const lan = user.company.details.language
+      const lanCode = lan ? languageMapper(lan) : 'en'
+      i18n.changeLanguage(lanCode)
+    }
+  }, [user, i18n])
 
   const schema: Schema = {
     full: t('marketingsource-title.translation'),
@@ -126,7 +132,7 @@ export const Index: NextPage = () => {
     },
     deleteBtnLabel: 'Yes, Delete Source',
     fields: {
-      source_name: {
+      name: {
         full: 'Friendly Name',
         fullLower: 'friendly name',
         short: t('marketingsource-name-textfield.translation'),
@@ -138,10 +144,10 @@ export const Index: NextPage = () => {
         cssWidth: 'max',
         type: 'string',
       },
-      public: {
+      is_active: {
         full: t('marketingsource-tableColumn-active.translation'),
-        type: 'number',
-        defaultvalue: 1,
+        type: 'boolean',
+        defaultvalue: true,
       },
     },
   }
