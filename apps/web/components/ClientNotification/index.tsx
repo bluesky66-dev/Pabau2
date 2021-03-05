e9ddceb7e7cc584a16deffd21741e0d8c721bd03
rinki singh <rinkisinghdevninja@gmail.com>
2021-03-05 14:31:24 +0530

    PAB2-480 lead responses-updated with master

--- apps/web/components/ClientNotification/index.tsx
+++ apps/web/components/ClientNotification/index.tsx
@@ -6,7 +6,7 @@ import NewAppointmentPreview from '../ClientNotificationPreview/NewAppointmentPr
 import RescheduleAppointmentPreview from '../ClientNotificationPreview/RescheduleAppointmentPreview'
 import BookedOntoClassPreview from '../ClientNotificationPreview/BookedOntoClassPreview'
 import WaitListPreview from '../ClientNotificationPreview/WaitListPreview'
-import LeadResponsesPreview from '../../pages/client-notifications/lead-responses/LeadResponsesPreview'
+import LeadResponsesPreview from '../ClientNotificationPreview/LeadResponsesPreview'
 
 interface P {
   onSeletedTab: (number) => void
