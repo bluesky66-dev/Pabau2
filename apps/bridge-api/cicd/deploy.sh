680cdcd7cc8b06b58b83e7911f8fe5ec0bc81633
tboshkovski <toshe@pabau.com>
2021-03-06 19:37:41 +0100

    authentication changes

--- apps/bridge-api/cicd/deploy.sh
+++ apps/bridge-api/cicd/deploy.sh
@@ -2,9 +2,6 @@
 set -e
 APP_NAME="$(basename "$(dirname "$(cd "$(dirname "${0}")"; pwd)")")"
 
-DOCKER_HOSTNAME="registry.pabau.com"
-DOCKER_USERNAME="ops"
-DOCKER_PASSWORD="jfdjiwfdjwiojdsmcnvhsbakdujr"
 
 echo "DEBUG: pwd=$(pwd)"
 echo "DEBUG: app_name=${APP_NAME}"
@@ -12,9 +9,9 @@ echo "DEBUG: DOCKER_HOSTNAME=${DOCKER_HOSTNAME}"
 echo "DEBUG: DOCKER_USERNAME=${DOCKER_USERNAME}"
 echo "DEBUG: DOCKER_PASSWORD=${DOCKER_PASSWORD}"
 
-# TODO!!
-#echo "ABORTING UNTIL https://github.com/prisma/prisma/issues/5304 IS FIXED!!!!"
-#exit;
+ TODO!!
+echo "ABORTING UNTIL https://github.com/prisma/prisma/issues/5304 IS FIXED!!!!"
+exit;
 
 echo "Building..."
 yarn run nx run "${APP_NAME}:build" --prod
--- apps/bridge-api/src/app/authentication/AuthenticationService.ts
+++ apps/bridge-api/src/app/authentication/AuthenticationService.ts
@@ -12,17 +12,17 @@ export class AuthenticationService {
   public constructor(private ctx: Context, private loginInput:LoginInputDto) {}
   //TODO Refactor once company select screen is defined
   public async handleLoginRequest(): Promise<string> {
-    const hash = await this.generateHash(this.loginInput.password, 'md5', 'hex');
-    this.user = await this.ctx.prisma.user.findMany({
+    const users: User[] =  await this.ctx.prisma.user.findMany({
       where: {
         username: {
           equals: this.loginInput.username
         },
-        password: {
-          equals: hash
-        }
       },
     })
+    this.user = users.filter(currentUser => {
+      return Promise.resolve(this.verifyPassword(currentUser, this.loginInput.password))
+    })
+    console.log((this.user))
     if(!this.user || Object.getOwnPropertyNames(this.user).length === 0){
       throw new Error('Unauthorized access');
     }
@@ -30,6 +30,15 @@ export class AuthenticationService {
   }
   protected async generateHash(password:string, encryption:string, encoding:BinaryToTextEncoding): Promise<string>{
       return crypto.createHash(encryption).update(password).digest(encoding);
+  }
+
+  private async verifyPassword(user: User, password: string): Promise<string> {
+    if(user.password_algor === 1){
+      return this.generateHash(password, 'md5', 'hex');
+    } if (user.password_algor === 2) {
+      return this.generateHash(user.salt + password + user.salt, 'sha1', 'hex');
+    }
+    throw new Error('Password algorithm not supported')
   }
   private async generateJWT(key: string): Promise<string>{
     return jwt.sign(<JwtPayloadDto> {
