ffa25f2fae29b210b46a98401f8ccfd6c1b1917e
Rocky Wang <wang.mythaero@gmail.com>
2021-03-08 10:19:49 -0800

    Update SB for CreateService

--- libs/ui/src/lib/create-service/CreateService.stories.tsx
+++ libs/ui/src/lib/create-service/CreateService.stories.tsx
@@ -1,10 +1,65 @@
-import React, { FC, useState } from 'react'
-import { Button } from '@pabau/ui'
-import { CreateService } from './CreateService'
+import React, { FC } from 'react'
+import { CreateService, CreateServiceProps } from './CreateService'
 import theLondonClinic from '../../assets/images/the-london-clinic.png'
 import sloanMedicalCenter from '../../assets/images/sloan-medical-center.png'
 import { ReactComponent as Bupa } from '../../assets/images/bupa.svg'
 import { ReactComponent as AxaPPP } from '../../assets/images/axa-ppp.svg'
+
+const rooms = ['Botox', ' Theraphy', 'Massage', 'Lab']
+const equipment = [
+  'Equipment A',
+  'Equipment B',
+  'Equipment C',
+  'Equipment D',
+  'Equipment E',
+  'Equipment F',
+  'Equipment G',
+]
+const contracts = [
+  {
+    logo: <Bupa />,
+    name: 'BUPA',
+    type: 'Insurance',
+  },
+  {
+    logo: <AxaPPP />,
+    name: 'AXA PPP',
+    type: 'Insurance',
+  },
+]
+const employees = [
+  { name: 'Jessica Winter', selected: false },
+  { name: 'Jeff Hackley', selected: false },
+  { name: 'Alexander Wang', selected: false },
+  { name: 'Linda Davis', selected: false },
+  { name: 'William Tyson', selected: false },
+  { name: 'Max Starck', selected: false },
+  { name: 'Kyle Walsh', selected: false },
+  { name: 'Owen Phillips', selected: false },
+  { name: 'Aidan Kelly', selected: false },
+  { name: 'Ewan Morgan', selected: false },
+  { name: 'Jordan Martin', selected: false },
+  { name: 'Grant Dudley', selected: false },
+]
+const locations = [
+  {
+    location: 'The London Clinic',
+    detail: '20 Devonshire Pl, Marylebone, London W1G 6BW, UK',
+    selected: false,
+    img: theLondonClinic,
+  },
+  {
+    location: 'Sloan Medical Centre',
+    detail: '2 Little London Rd, Meersbrook, Sheffield S8 0YH, UK',
+    selected: false,
+    img: sloanMedicalCenter,
+  },
+  {
+    location: 'Sheffield Late Night Pharmacy',
+    detail: '277 Fulwood Rd, Sheffield S10 3BD, UK',
+    selected: false,
+  },
+]
 
 export default {
   title: 'Modals/CreateService',
@@ -15,72 +70,16 @@ export default {
   },
 }
 
-export const CreateServiceStory: FC = () => {
-  const [visible, setVisible] = useState(false)
-  return (
-    <>
-      <Button type="primary" onClick={() => setVisible(true)}>
-        Create service
-      </Button>
-      <CreateService
-        rooms={['Botox', ' Theraphy', 'Massage', 'Lab']}
-        equipment={[
-          'Equipment A',
-          'Equipment B',
-          'Equipment C',
-          'Equipment D',
-          'Equipment E',
-          'Equipment F',
-          'Equipment G',
-        ]}
-        contracts={[
-          {
-            logo: <Bupa />,
-            name: 'BUPA',
-            type: 'Insurance',
-          },
-          {
-            logo: <AxaPPP />,
-            name: 'AXA PPP',
-            type: 'Insurance',
-          },
-        ]}
-        employees={[
-          { name: 'Jessica Winter', selected: false },
-          { name: 'Jeff Hackley', selected: false },
-          { name: 'Alexander Wang', selected: false },
-          { name: 'Linda Davis', selected: false },
-          { name: 'William Tyson', selected: false },
-          { name: 'Max Starck', selected: false },
-          { name: 'Kyle Walsh', selected: false },
-          { name: 'Owen Phillips', selected: false },
-          { name: 'Aidan Kelly', selected: false },
-          { name: 'Ewan Morgan', selected: false },
-          { name: 'Jordan Martin', selected: false },
-          { name: 'Grant Dudley', selected: false },
-        ]}
-        locations={[
-          {
-            location: 'The London Clinic',
-            detail: '20 Devonshire Pl, Marylebone, London W1G 6BW, UK',
-            selected: false,
-            img: theLondonClinic,
-          },
-          {
-            location: 'Sloan Medical Centre',
-            detail: '2 Little London Rd, Meersbrook, Sheffield S8 0YH, UK',
-            selected: false,
-            img: sloanMedicalCenter,
-          },
-          {
-            location: 'Sheffield Late Night Pharmacy',
-            detail: '277 Fulwood Rd, Sheffield S10 3BD, UK',
-            selected: false,
-          },
-        ]}
-        visible={visible}
-        onClose={() => setVisible(false)}
-      />
-    </>
-  )
+const CreateServiceStory: FC<CreateServiceProps> = ({ ...args }) => {
+  return <CreateService {...args} />
+}
+
+export const Default = CreateServiceStory.bind({})
+Default.args = {
+  visible: false,
+  rooms,
+  equipment,
+  contracts,
+  employees,
+  locations,
 }
