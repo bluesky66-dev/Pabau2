ab3947431d3486c313b03ccb89e6c2a1064ff62e
D.A. Zhen <dokam2050@gmail.com>
2021-03-08 01:08:43 -0500

    PAB2-429

--- libs/ui/src/lib/medicalformbuilder/MedicalFormPreview.tsx
+++ libs/ui/src/lib/medicalformbuilder/MedicalFormPreview.tsx
@@ -1,38 +1,6 @@
 import { MedicalFormPreview as MedicalFormPreviewPanel } from '@pabau/ui'
 import React, { FC } from 'react'
-import iconHome from '../../assets/images/share.svg'
 import styles from './MedicalFormBuilder.module.less'
-
-const data = [
-  {
-    step: 1,
-    name: 'Check Details',
-    imgPath: iconHome,
-    isActive: true,
-    index: 0,
-  },
-  {
-    step: 2,
-    name: 'History',
-    imgPath: iconHome,
-    isActive: true,
-    index: 1,
-  },
-  {
-    step: 3,
-    name: 'Consent',
-    imgPath: iconHome,
-    isActive: false,
-    index: 2,
-  },
-  {
-    step: 4,
-    name: 'Complete',
-    imgPath: iconHome,
-    isActive: false,
-    index: 3,
-  },
-]
 
 const MedicalFormPreview: FC = () => {
   return (
